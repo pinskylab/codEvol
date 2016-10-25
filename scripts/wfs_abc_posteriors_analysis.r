610a516ccd4fbe571345a55ccb5c3b41fdcca8d8
Malin Pinsky <malin.pinsky@gmail.com>
2016-10-25 14:02:41 +0200

    Adding plots

--- scripts/wfs_abc_posteriors_analysis.r
+++ scripts/wfs_abc_posteriors_analysis.r
@@ -7,6 +7,7 @@ if(!grepl('hpc.uio.no', Sys.info()["nodename"])){
 	require(RColorBrewer)
 	require(data.table)
 	require(boa)
+	require(hexbin)
 }
 if(grepl('hpc.uio.no', Sys.info()["nodename"])){
 	require(RColorBrewer, lib.loc="/projects/cees/lib/R_packages/")
@@ -26,6 +27,7 @@ mcip <- function(x){ # mean, CIs, and p(x<>0) for an MCMC chain
 	return(c(mean=mean(x), l95=b[1], u95=b[2], p=p))
 }
 
+rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
 
 # load data
 load('analysis/wfs_abc_posts.rdata') # posteriors from ABC "posts"
@@ -74,25 +76,27 @@ neginds <- hpds$s$p.adj < 0.05 & hpds$s$mean < 0
 sum(posinds) # number of loci
 sum(neginds)
 
-
+# Examine candidates
+print(locnms[neginds,], nrow=sum(neginds))
+print(locnms[posinds,], nrow=sum(posinds))
 
 ################################
-# plot
+# plots
 ################################
 
 colrmp <- colorRamp(colors=brewer.pal(11, 'RdYlBu'))
 getcol <- function(x, alpha=1){ rgbs <-colrmp(x); return(rgb(rgbs[,1], rgbs[,2], rgbs[,3], alpha*256, maxColorValue=256))}
 
-	# Initial frequency vs. posterior s
+# Initial frequency vs. posterior s
 plot(hpds$f1$mean, hpds$s$mean, xlab='Initial frequency', ylab='Posterior mean s', col=getcol(hpds$s$mean/2+0.5))
 points(hpds$f1$mean[neginds|posinds], hpds$s$mean[neginds|posinds], xlab='Initial frequency', ylab='Posterior mean s', pch=16)
 
-	# Obs init freq vs. posterior init freq
+# Obs init freq vs. posterior init freq
 plot(obsfrqs$f1, hpds$f1$mean, col=getcol(hpds$s$mean/2+0.5, 0.2), xlab='Observed initial frequency', ylab='Posterior mean initial frequency')
 points(obsfrqs$f1[neginds|posinds], hpds$f1$mean[neginds|posinds], pch=16)
 
-	# Initial frequency vs. change in frequency vs. posterior s
-	# only for loci "under selection"
+# Initial frequency vs. change in frequency vs. posterior s
+# overplot loci "under selection"
 inds <- neginds | posinds
 	
 quartz(width=7, height=6)
@@ -103,6 +107,12 @@ points(obsfrqs$f1[inds], obsfrqs$diff[inds], col='black', pch=16, cex=0.8)
 plot(rep(1,100),seq(-1,1,length.out=100), col=getcol(seq(-1,1,length.out=100)/2+0.5), bty='n', xaxt='n', ylab='Posterior mean s', xlab='', pch=15, cex=0.7) # legend
 
 
-# Examine candidates
-print(locnms[neginds,], nrow=sum(neginds))
-print(locnms[posinds,], nrow=sum(posinds))
+# Examine MCMC sample distributions for individual loci
+i <- which(obsfrqs$f1 > 0.8 & obsfrqs$f1 < 0.90 & obsfrqs$diff < -0.25) # initially high frequency loci under negative selection
+j <- 2 # pick which one
+plot(density(posts$s[i[j],]))
+plot(density(posts$f1[i[j],])); abline
+
+hexbinplot(posts$s[i[j],] ~ posts$f1[i[j],], colramp=rf, main=paste('locus', i[j]), xlab='posterior mean initial frequency', ylab='posterior mean s')
+
+hexbinplot(posts$s[i[j],] ~ posts$ne[i[j],], colramp=rf, main=paste('locus', i[j]), xlab='posterior mean Ne', ylab='posterior mean s')
