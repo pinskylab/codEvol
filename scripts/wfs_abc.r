83bc6bef16ee5a0d7920f410deae2b559fc12d65
Malin Pinsky <malin.pinsky@gmail.com>
2017-02-05 22:31:50 -0500

    Continuing migration to foreach and ff
    Parallel code seems to work on a single slice of out (out.ff)
    Next need to set up all the ff files and normalize the summary stats in
    each, so that they can be used by foreach

--- scripts/wfs_abc.r
+++ scripts/wfs_abc.r
@@ -13,6 +13,7 @@ if(grepl('hpc.uio.no', Sys.info()["nodename"])){
 	require(iterators, lib.loc="/projects/cees/lib/R_packages/") # used with foreach
 	require(foreach, lib.loc="/projects/cees/lib/R_packages/") # for parallel procssing using foreach()
 	require(doMC, lib.loc="/projects/cees/lib/R_packages/")
+	require(doParallel, lib.loc="/projects/cees/lib/R_packages/") # needed for foreach
 	require(doSNOW, lib.loc="/projects/cees/lib/R_packages/") # set up SOCK cluster
 	require(bit, lib.loc="/projects/cees/lib/R_packages/") # for use with ff
 	require(ff, lib.loc="/projects/cees/lib/R_packages/") # for big objects shared across processes
@@ -28,6 +29,8 @@ load('analysis/wfs_sims.rdata') # load the simulations: 'out' array. slow to loa
 obs <- fread('analysis/LOF_07_to_LOF_S_14.w_obs_stats.txt') # the observations of Fsi and Fsd
 dat <- fread('analysis/LOF_07_to_LOF_S_14.wfabc', skip=2) # the data on samples sizes and observed allele frequencies
 
+# parameters
+tol <- 0.001 # 10k from 10M sims
 
 # prep data
 sampsize <- dat[seq(1,nrow(dat),by=2),] # sample sizes in # of chromosomes
@@ -35,51 +38,31 @@ alcnts <- dat[seq(2,nrow(dat),by=2),] # allele counts in # chromosomes
 
 # abc function
 # out.ff is an ff matrix (one slice of out[,,i])
-runabc <- function(allobsrow, out.ff){
-	locusnum <- as.numeric(allobsrow)[1]
-	sampsizerow <- as.numeric(allobsrow)[2:3]
-	alcntsrow <- as.numeric(allobsrow)[4:5]
-	obsrow <- as.numeric(allobsrow)[6:11]
+runabc <- function(locusnum, thistarg, out.ff, tol){
 
-#	j <- which(dimnames(out)[[3]] == paste(sampsizerow, collapse=',')) # find the slice of out that has simulations with the equivalent sample size
-#	if(length(j)==1){
-	if(length(dim(out.ff))==2){
-		targ <- c(alcntsrow[1]/sampsizerow[1], obsrow[2:1]) # f1samp, fsd, fsi
-		pams <- as.data.table(t(out.ff[c(1,2,3),])) # ne, f1, s
-		stats <- as.data.frame(t(out[c(6,8,9),])) # f1samp, fsd, fsi
-#		pams <- as.data.frame(t(out[c(1,2,3),,j])) # ne, f1, s
-#		stats <- as.data.frame(t(out[c(6,8,9),,j])) # f1samp, fsd, fsi
-
-
-		# with f1samp as a summary stat
-#		post <- abc(target=targ, param=pams, sumstat=stats, tol=0.001, method='rejection')
-
-		# do by hand
-    
-		# calc euclidean distance
-
-			sum1 <- 0
-			for(j in 1:nss){
-				sum1 <- sum1 + (scaled.sumstat[,j]-target.s[j])^2
-		   }
-		   dst <- sqrt(sum1)
-		# includes the effect of gwt in the tolerance
-			dst[!gwt] <- floor(max(dst[gwt])+10)
+	# with f1samp as a summary stat
+	# calc euclidean distance
+	sum1 <- (out.ff['f1samp',] - thistarg[,f1samp.n])^2
+	sum1 <- sum1 + (out.ff['fsdprime',] - thistarg[,fsd.n])^2
+	sum1 <- sum1 + (out.ff['fsiprime',] - thistarg[,fsi.n])^2
+	dst <- sqrt(sum1)
 	
-
-		# wt1 defines the region we're interested in 
-			abstol <- quantile(dst,tol)
-			wt1 <- dst < abstol
- 
-
-
-		ret <- matrix(data=c(rep(locusnum, nrow(post$ss)), post$ss[,1], post$ss[,2], post$ss[,3], post$unadj.values[,1], post$unadj.values[,2], post$unadj.values[,3]), ncol=7, byrow=FALSE)
-		colnames(ret) <- c('locus', 'f1samp', 'fsdprime', 'fsiprime', 'ne', 'f1', 's')
-
-		return(ret)
-	} else {
-		warning(paste("Didn't have sample size sims for row", locusnum))
+	# includes the effect of gwt in the tolerance
+	gwt <- !is.na(dst) # for now, simply define gwt as value that aren't NA
+	dst[!gwt] <- floor(max(dst[gwt])+10)
+	
+	# wt1 defines the region we're interested in 
+	abstol <- quantile(dst,tol)
+	wt1 <- dst < abstol
+	if(sum(wt1)==0){ # can happen if min(dst) == abstol
+		wt1 <- dst <= abstol
 	}
+ 
+	# set up return matrix
+	ret <- matrix(data=c(rep(locusnum, sum(wt1)), out.ff['f1samp',wt1], out.ff['fsdprime',wt1], out.ff['fsiprime',wt1], out.ff['ne',wt1], out.ff['f1',wt1], out.ff['s',wt1]), ncol=7, byrow=FALSE)
+	colnames(ret) <- c('locus', 'f1samp', 'fsdprime', 'fsiprime', 'ne', 'f1', 's')
+
+	return(ret)
 }
 
 ####################################################
@@ -129,64 +112,91 @@ runabc <- function(allobsrow, out.ff){
 ###################################################
 ### run calculations in parallel with foreach CURRENTLY USES TOO MUCH MEMORY
 ###################################################
-# set up cluster
-#registerDoMC(15) # register cores. seems to use too much memory (creates 15 new cores on every loop)
-cl <- makeCluster(15, type='SOCK')
-registerDoSNOW(cl)
 
-# set up data chunks to operate on and write out separately
-lociperpart <- 15 # how many loci in each part (each part will be written to file)
-parts <- 1:ceiling(nrow(sampsize)/lociperpart) # 1 to number of parts
-print(paste(length(parts), "parts"))
-#partinds <- rep(parts, rep(lociperpart, length(parts))) # index for each row to each part
-partinds <- vector('list', length(parts)); for(i in 1:length(partinds)) partinds[[i]] <- ((i-1)*lociperpart+1):min(c(i*lociperpart, nrow(sampsize))) # indices for the loci in each part
-ndigits <- nchar(as.character(nrow(sampsize))) # used for formatting file names
 
 # set up target statistics
-targ <- cbind(alcnts[,V1]/sampsize[,V1], obs[,.(Fsd, Fsi)]) # f1samp, fsd, fsi
+	# stats for each locus
+	targ <- cbind(sampsize[,.(V1, V2)], alcnts[,V1]/sampsize[,V1], obs[,.(Fsd, Fsi)]) # alcnt1, alcnt2, f1samp, fsd, fsi
+	setnames(targ, 1:5, c('alcnt1', 'alcnt2', 'f1samp', 'fsd', 'fsi'))
+	
 	# scale by the appropriate summary statistic
 	# need mean and sd for each summary stat in out
-	uniqsamps <- apply(do.call('rbind', strsplit(dimnames(out)[[3]], split=',')), 2, as.numeric) # extract unique sample sizes
+	uniqsamps <- apply(do.call('rbind', strsplit(dimnames(out)[[3]], split=',')), 2, as.numeric) # extract unique sample sizes. could also have extracted (probably more easily) from targ
 	meansds <- data.table(alcnt1=uniqsamps[,1], alcnt2=uniqsamps[,2])
-	meansds[,f1samp.mean:=NA]
-	meansds[,f1samp.sd:=NA]
-	meansds[,fsd.mean:=NA]
-	meansds[,fsd.sd:=NA]
-	meansds[,fsi.mean:=NA]
-	meansds[,fsi.sd:=NA]
-	for(i in 1:nrow(meansds)){ 
+	meansds[,f1samp.mean:=numeric(nrow(meansds))]
+	meansds[,f1samp.sd:=numeric(nrow(meansds))]
+	meansds[,fsd.mean:=numeric(nrow(meansds))]
+	meansds[,fsd.sd:=numeric(nrow(meansds))]
+	meansds[,fsi.mean:=numeric(nrow(meansds))]
+	meansds[,fsi.sd:=numeric(nrow(meansds))]
+	for(i in 1:nrow(meansds)){ # this takes 22%+ of a cod node RAM... why? perhaps because mean and sd on vectors of length 10M
 		print(i)
 		j <- which(dimnames(out)[[3]] == paste(meansds[i,.(alcnt1, alcnt2)], collapse=',')) # have to find the slice of out that matches the sample sizes for this row
-		meansds[i,f1samp.mean:=mean(out['f1samp',,j])]
-		meansds[i,f1samp.sd:=sd(out['f1samp',,j])]
-		meansds[i,fsd.mean:=mean(out['fsdprime',,j])]
-		meansds[i,fds.sd:=sd(out['fsdprime',,j])]
-		meansds[i,fsi.mean:=mean(out['fsiprime',,j])]
-		meansds[i,fsi.sd:=sd(out['fsiprime',,j])]
-	}
+		meansds[i,f1samp.mean:=mean(out['f1samp',,j], na.rm=TRUE)]
+		meansds[i,f1samp.sd:=sd(out['f1samp',,j], na.rm=TRUE)]
+		meansds[i,fsd.mean:=mean(out['fsdprime',,j], na.rm=TRUE)]
+		meansds[i,fsd.sd:=sd(out['fsdprime',,j], na.rm=TRUE)]
+		meansds[i,fsi.mean:=mean(out['fsiprime',,j], na.rm=TRUE)]
+		meansds[i,fsi.sd:=sd(out['fsiprime',,j], na.rm=TRUE)]
+	}	
+	meansds[f1samp.sd==0, f1samp.sd:=1] # set sd = 1 for cases were sd=0, to prevent Inf
+	meansds[fsd.sd==0, fsd.sd:=1] # set sd = 1 for cases were sd=0, to prevent Inf
+	meansds[fsi.sd==0, fsi.sd:=1] # set sd = 1 for cases were sd=0, to prevent Inf
 	
-for(i in 1:nrow(out.ff)){
-	targ[j] <- normalise(targ[j],sumstat[,j][gwt])
-}
+	# add mean and sd to targ to allow easy normalization
+	setkey(targ, alcnt1, alcnt2)
+	setkey(meansds, alcnt1, alcnt2)
+	targ <- meansds[targ,]
+	
+	# normalize the target stats
+	targ[,f1samp.n:=(f1samp-f1samp.mean)/f1samp.sd]
+	targ[,fsd.n:=(fsd-fsd.mean)/fsd.sd]
+	targ[,fsi.n:=(fsi-fsi.mean)/fsi.sd]
+
+	# write out?
+	save(targ, file='analysis/targ.rdata')	
 
 # export out file from abc sims to ff data type for memory-efficient use across cluster
-outszs <- dimnames(out)[[3]] # sample sizes
+	outszs <- dimnames(out)[[3]] # sample sizes
 
-out.ff <- ff(out[,,5], dim=dim(out[,,5]), filename=paste(getwd(), '/analysis/temp/wfs_sims_ff', paste(sampsize[1,], collapse=','), sep=''), finalizer='delete')
-	# scale everything
-	for(i in 1:nrow(out.ff)){
-		out.ff[i,] <- normalise(sumstat[,j],sumstat[,j][gwt])
+	# will need to do this for all 193 combinations of sample sizes
+	i = 1
+		j <- which(dimnames(out)[[3]] == paste(meansds[i,.(alcnt1, alcnt2)], collapse=',')) # have to find the slice of out that matches the sample sizes for this row
+		out.ff <- ff(out[,,j], dim=dim(out[,,j]), dimnames=dimnames(out[,,j]), filename=paste(getwd(), '/analysis/temp/wfs_sims_ff', paste(meansds[i,.(alcnt1, alcnt2)], collapse=','), sep=''), finalizer='close')
+
+		# scale everything
+		out.ff['f1samp',] <- (out.ff['f1samp',]-meansds[i,f1samp.mean])/meansds[i,f1samp.sd]
+		out.ff['fsdprime',] <- (out.ff['fsdprime',]-meansds[i,fsd.mean])/meansds[i,fsd.sd]
+		out.ff['fsiprime',] <- (out.ff['fsiprime',]-meansds[i,fsi.mean])/meansds[i,fsi.sd]
 	}
+
+## parts before this can be in a data prep script
+
+# set up data chunks to operate on and write out separately
+lociperpart <- 2 # how many loci in each part (each part will be written to file)
+parts <- 1:ceiling(nrow(targ)/lociperpart) # 1 to number of parts
+print(paste(length(parts), "parts"))
+#partinds <- rep(parts, rep(lociperpart, length(parts))) # index for each row to each part
+partinds <- vector('list', length(parts)); for(i in 1:length(partinds)) partinds[[i]] <- ((i-1)*lociperpart+1):min(c(i*lociperpart, nrow(targ))) # indices for the loci in each part
+ndigits <- nchar(as.character(nrow(targ))) # used for formatting file names
+
+# set up cluster
+#registerDoMC(2) # register cores. doesn't clean up after itself (creates 15 new cores on every loop)
+.libPaths('/projects/cees/lib/R_packages/')
+cl <- makeCluster(2, type='SOCK')
+registerDoSNOW(cl)
+clusterCall(cl, function(x) .libPaths(x), .libPaths()) # add libpaths to each node
 
 #for(partnum in parts){
 for(partnum in 1:2){
-print(paste('partnum', partnum, 'of', length(parts)))
+	print(paste('partnum', partnum, 'of', length(parts)))
 	thisdat <- iter(cbind((1:nrow(sampsize))[partinds[[partnum]]], targ[partinds[[partnum]],]), by='row') # the iterator object: this part of the loop's chunk of data
 	
-	results <- foreach(i=thisdat, .combine=rbind, .packages=c('ff', 'abc', 'abc.data')) %dopar% {
-		j <- which(dimnames(out)[[3]] == paste(sampsize[i,], collapse=',')) # have to find the slice of out that matches the sample sizes for this row
-		# have to pick the ff object here
-		runabc(i, out.ff) 
+	results <- foreach(i=thisdat, .combine=rbind, .packages=c('ff', 'doParallel', 'data.table')) %dopar% {
+		# have to pick the correct ff object here (there will be 193)
+#		j <- which(dimnames(out)[[3]] == paste(sampsize[i,], collapse=',')) # have to find the slice of out that matches the sample sizes for this row
+		# then run abc
+		runabc(locusnum=i[,V1], thistarg=i[,.(f1samp.n, fsd.n, fsi.n)], out.ff, tol=tol) 
 	}
 
 	minloc <- formatC(min(partinds[[partnum]]), width=ndigits, flag='0')
