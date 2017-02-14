95d6214768bf16703c33eeb3c04f038ca6ac0913
Malin Pinsky <malin.pinsky@gmail.com>
2017-02-14 07:43:22 -0500

    Plots to check that the ABC simulations are working

--- scripts/wfs_abc_checksims.r
+++ scripts/wfs_abc_checksims.r
@@ -16,7 +16,8 @@ if(grepl('hpc.uio.no', Sys.info()["nodename"])){
 }
 
 # the abc simulations
-ffnm <- paste('analysis/temp/wfs_sims_ff', '44,44', sep='')
+sampsize <- '44,44'
+ffnm <- paste('analysis/temp/wfs_sims_ff', sampsize, sep='')
 ffload(ffnm, overwrite=TRUE) # takes 30 sec or so. loads thisout.ff
 
 head(thisout.ff)
@@ -28,7 +29,7 @@ meansds <- meansds[alcnt1==44 & alcnt2==44,]
 
 
 ### basic plots
-i <- 1:2000 # selection a few simulations to simplify plotting
+i <- 1:2000 # select a few simulations to simplify plotting
 
 # fsi and fsd vs. diff and f1samp
 pdf('analysis/figures/wfs_abc_sims_fs_vs_f1samp_diff.pdf')
@@ -58,11 +59,11 @@ dev.off()
 	# in slices of f1samp
 i <- 1:10000 # selection a few simulations to simplify plotting
 
-pdf('analysis/figures/wfs_abc_sims_s_vs_diff_by_f1samp.pdf', width=9, height=4)
+pdf('analysis/figures/wfs_abc_sims_s_vs_obsdiff_by_f1samp.pdf', width=9, height=4)
 par(mfrow=c(2,5))
 ints <- list(c(0,0.1), c(0.1, 0.2), c(0.2, 0.3), c(0.3, 0.4), c(0.4, 0.5), c(0.5, 0.6), c(0.6, 0.7), c(0.7, 0.8), c(0.8, 0.9), c(0.9, 1))
 for(j in 1:length(ints)){
-	k <- which(thisout.ff['f1samp',i] > ints[[j]][1] & thisout.ff['f1samp',i] < ints[[j]][2])
+	k <- which((thisout.ff['f1samp',i]*meansds$f1samp.sd + meansds$f1samp.mean) > ints[[j]][1] & (thisout.ff['f1samp',i]*meansds$f1samp.sd + meansds$f1samp.mean) < ints[[j]][2])
 	plot(thisout.ff['f2samp',i][k]-thisout.ff['f1samp',i][k]*meansds$f1samp.sd - meansds$f1samp.mean, thisout.ff['s', i][k], xlab='diff', ylab='s', main=paste('f1samp', paste(ints[[j]], collapse='-')), xlim=c(-1,1), ylim=c(-1,1), cex=0.5)
 }
 dev.off()
@@ -81,3 +82,53 @@ for(j in 1:length(ints)){
 }
 dev.off()
 
+# s vs. f2 in slices of f1
+i <- 1:10000 # selection a few simulations to simplify plotting
+
+pdf('analysis/figures/wfs_abc_sims_s_vs_f2_by_f1.pdf', width=9, height=4)
+par(mfrow=c(2,5))
+ints <- list(c(0,0.1), c(0.1, 0.2), c(0.2, 0.3), c(0.3, 0.4), c(0.4, 0.5), c(0.5, 0.6), c(0.6, 0.7), c(0.7, 0.8), c(0.8, 0.9), c(0.9, 1))
+for(j in 1:length(ints)){
+	k <- which(thisout.ff['f1',i] > ints[[j]][1] & thisout.ff['f1',i] < ints[[j]][2])
+	plot(thisout.ff['f2',i][k], thisout.ff['s', i][k], xlab='f2', ylab='s', main=paste('f1', paste(ints[[j]], collapse='-')), xlim=c(0,1), ylim=c(-1,1), cex=0.5, col=rgb(0.2, 0.2, 0.2, 0.2))
+}
+dev.off()
+
+# s vs. f2samp in slices of f1samp
+i <- 1:10000 # selection a few simulations to simplify plotting
+
+pdf('analysis/figures/wfs_abc_sims_s_vs_f2samp_by_f1samp.pdf', width=9, height=4)
+par(mfrow=c(2,5))
+ints <- list(c(0,0.1), c(0.1, 0.2), c(0.2, 0.3), c(0.3, 0.4), c(0.4, 0.5), c(0.5, 0.6), c(0.6, 0.7), c(0.7, 0.8), c(0.8, 0.9), c(0.9, 1))
+f1samp <- thisout.ff['f1samp',i]*meansds$f1samp.sd + meansds$f1samp.mean
+f2samp <- thisout.ff['f2samp',i]
+s <- thisout.ff['s',i]
+for(j in 1:length(ints)){
+	k <- which(f1samp > ints[[j]][1] & f1samp < ints[[j]][2])
+	plot(f2samp[k], s[k], xlab='f2samp', ylab='s', main=paste('f1samp', paste(ints[[j]], collapse='-')), xlim=c(0,1), ylim=c(-1,1), cex=0.5, col=rgb(0.2, 0.2, 0.2, 0.2))
+}
+dev.off()
+
+
+
+# s vs. fsi in slices of f1samp
+i <- 1:10000 # selection a few simulations to simplify plotting
+
+pdf('analysis/figures/wfs_abc_sims_s_vs_fsi_by_f1samp.pdf', width=9, height=4)
+par(mfrow=c(2,5))
+ints <- list(c(0,0.1), c(0.1, 0.2), c(0.2, 0.3), c(0.3, 0.4), c(0.4, 0.5), c(0.5, 0.6), c(0.6, 0.7), c(0.7, 0.8), c(0.8, 0.9), c(0.9, 1))
+f1samp <- thisout.ff['f1samp',i]*meansds$f1samp.sd + meansds$f1samp.mean
+fsi <- thisout.ff['fsiprime',i]*meansds$fsi.sd + meansds$fsi.mean
+s <- thisout.ff['s',i]
+xlims <- range(fsi, na.rm=TRUE)
+for(j in 1:length(ints)){
+	k <- which(f1samp > ints[[j]][1] & f1samp < ints[[j]][2])
+	plot(fsi[k], s[k], xlab='fsi', ylab='s', main=paste('f1samp', paste(ints[[j]], collapse='-')), xlim=xlims, ylim=c(-1,1), cex=0.5, col=rgb(0.2, 0.2, 0.2, 0.2))
+}
+dev.off()
+
+
+
+## clean up
+	delete(thisout.ff)
+	rm(thisout.ff)
