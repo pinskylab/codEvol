67ab2b379e3296bd36c9a2564b7609e6da2fa266
Malin Pinsky <malin.pinsky@gmail.com>
2017-02-14 13:38:47 -0500

    Test ABC with f1samp,fsi,fsd vs. f1samp,f2samp
    Script to run a single locus by hand

--- scripts/wfs_abc_testf1sampf2samp.r
+++ scripts/wfs_abc_testf1sampf2samp.r
@@ -33,11 +33,12 @@ ncores <- 10 # number of cores to use
 
 # abc function
 # thisout.ff is an ff matrix (one slice of out[,,i])
-runabc <- function(locusnum, thistarg, thisout.ff, tol){
+# assumes f1samp is not normalized in thisout.ff
+runabc <- function(locusnum, thistarg, thisout.ff, tol, f1samp.mean, f1samp.sd){
 
 	# with f1samp as a summary stat
 	# calc euclidean distance
-	sum1 <- (thisout.ff['f1samp',] - thistarg[,f1samp.n])^2
+	sum1 <- ((thisout.ff['f1samp',]-f1samp.mean)/f1samp.sd - thistarg[,f1samp.n])^2
 	sum1 <- sum1 + (thisout.ff['fsdprime',] - thistarg[,fsd.n])^2
 	sum1 <- sum1 + (thisout.ff['fsiprime',] - thistarg[,fsi.n])^2
 	dst <- sqrt(sum1)
@@ -60,12 +61,13 @@ runabc <- function(locusnum, thistarg, thisout.ff, tol){
 	return(ret)
 }
 
-# needs work! clunky because thisout.ff hasn't been reprocessed
-runabcf1f2 <- function(locusnum, thistarg, thisout.ff, tol, f1samp.sd, f1samp.mean){
+# using f1samp and f2samp
+# assumes neither have been normalized in thisout.ff
+runabcf1f2 <- function(locusnum, thistarg, thisout.ff, tol){
 
 	# with f1samp as a summary stat
 	# calc euclidean distance
-	sum1 <- (thisout.ff['f1samp',]*f1samp.sd+f1samp.mean - thistarg[,f1samp])^2
+	sum1 <- (thisout.ff['f1samp',] - thistarg[,f1samp])^2
 	sum1 <- sum1 + (thisout.ff['f2samp',] - thistarg[,f2samp])^2
 	dst <- sqrt(sum1)
 	
@@ -81,7 +83,7 @@ runabcf1f2 <- function(locusnum, thistarg, thisout.ff, tol, f1samp.sd, f1samp.me
 	}
  
 	# set up return matrix
-	ret <- matrix(data=c(rep(locusnum, sum(wt1)), thisout.ff['f1samp',wt1]*f1samp.sd+f1samp.mean, thisout.ff['f2samp',wt1], thisout.ff['fsdprime',wt1], thisout.ff['fsiprime',wt1], thisout.ff['ne',wt1], thisout.ff['f1',wt1], thisout.ff['s',wt1]), ncol=8, byrow=FALSE)
+	ret <- matrix(data=c(rep(locusnum, sum(wt1)), thisout.ff['f1samp',wt1], thisout.ff['f2samp',wt1], thisout.ff['fsdprime',wt1], thisout.ff['fsiprime',wt1], thisout.ff['ne',wt1], thisout.ff['f1',wt1], thisout.ff['s',wt1]), ncol=8, byrow=FALSE)
 	colnames(ret) <- c('locus', 'f1samp', 'f2samp', 'fsdprime', 'fsiprime', 'ne', 'f1', 's')
 
 	return(ret)
@@ -93,18 +95,21 @@ runabcf1f2 <- function(locusnum, thistarg, thisout.ff, tol, f1samp.sd, f1samp.me
 ### run calculations in parallel with foreach
 ###################################################
 
-locnum <- 7153
-sampsize <- '38,48'
+#locnum <- 37461; sampsize <- '38,40'
+locnum <- 7153; sampsize <- '38,48'
+
 meanind <- which(meansds$alcnt1 == 38 & meansds$alcnt2==48)
 
 ffnm <- paste('analysis/temp/wfs_sims_ff', sampsize, sep='')
 ffload(ffnm, overwrite=TRUE) # takes 30 sec or so. loads thisout.ff
 
+summary(thisout.ff['f1samp',]) # test whether f1samp was normalized
+
 i <- targ[locnum,] # the input object
 	
-results.fsi <- runabc(locusnum=i[,locusnum], thistarg=i[,.(f1samp.n, fsd.n, fsi.n)], thisout.ff=thisout.ff, tol=tol) # run abc analysis with f1samp and fsi/fsd (the old way)
+results.fsi <- runabc(locusnum=i[,locusnum], thistarg=i[,.(f1samp.n, fsd.n, fsi.n)], thisout.ff=thisout.ff, tol=tol, f1samp.mean=meansds$f1samp.mean[meanind], f1samp.sd=meansds$f1samp.sd[meanind]) # run abc analysis with f1samp and fsi/fsd (the old way)
 
-results.f1f2 <- runabcf1f2(locusnum=i[,locusnum], thistarg=i[,.(f1samp, f2samp)], thisout.ff=thisout.ff, tol=tol, f1samp.mean=meansds$f1samp.mean[meanind], f1samp.sd=meansds$f1samp.sd[meanind]) # run abc with unscaled f1samp and f2samp (the possible new way)
+results.f1f2 <- runabcf1f2(locusnum=i[,locusnum], thistarg=i[,.(f1samp, f2samp)], thisout.ff=thisout.ff, tol=tol) # run abc with unscaled f1samp and f2samp (the possible new way)
 
 
 	summary(results.fsi)
