09781b529eb8012f4561e64a6a0dce5e32e95c63
Malin Pinsky <malin.pinsky@gmail.com>
2017-07-09 21:45:51 -0400

    Update for analysis of 1907-2015 25k results

--- scripts/wfs_nullmodel_analysis.r
+++ scripts/wfs_nullmodel_analysis.r
@@ -6,8 +6,10 @@
 ## Prep 
 ##########
 require(data.table)
-locnms <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof14_25k.txt', header=TRUE); setnames(locnms, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')); suffix='_07-14_25k' # the name and observed frequencies of all the loci, from output by Bastiaan Star
+locnms <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof14_25k.txt', header=TRUE); setnames(locnms, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')) # the name and observed frequencies of all the loci, from output by Bastiaan Star
 load('analysis/wfs_nullmodel_pvals_07-14_25k.rdata') # dat
+suffix <- '_07-14_25k'
+locnms11 <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof11_25k.txt', header=TRUE); setnames(locnms11, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')) # for 1907-2011 (for comparison)
 
 # make a nucleotide position for the whole genome
 chrmax <- locnms[,.(len=max(POS)), by=CHROM]
@@ -37,14 +39,15 @@ setcolorder(dat, c('CHROM', 'POS', 'locusnum', 'POSgen', 'n', 'cnt1', 'cnt2', 'F
 # write out a nice version
 write.table(dat[,.(CHROM, POS, p.adj)], file=paste('analysis/wfs_nullmodel_pos&pvals', suffix, '.txt', sep=''), quote=FALSE, sep='\t', row.names=FALSE)
 
-# calculate a running mean -log10(p-value)
+# calculate a running mean -log10(p-value) (FDR-adjusted)
 stp = 1e5
-meanp <- data.frame(mids <- seq(stp/2, max(dat$POSgen), by=stp), mean <- rep(NA, length(mids))) # for mean of p-adj
+meanp <- data.frame(mids=seq(stp/2, max(dat$POSgen), by=stp), p=rep(NA, length(mids)), p.adj=rep(NA, length(mids))) # for mean of p and p-adj
 nrow(meanp)
 for(j in 1:nrow(meanp)){ # takes a couple minutes
 	if(j %% 100 == 0) cat(j)
 	inds <- dat$POSgen >= (meanp$mids[j]-stp/2) & dat$POSgen < (meanp$mids[j]+stp/2)
-	meanp$mean[j] <- mean(-log10(dat$p.adj[inds]))
+	meanp$p[j] <- mean(-log10(dat$p[inds]))
+	meanp$p.adj[j] <- mean(-log10(dat$p.adj[inds]))
 }
 
 
@@ -52,14 +55,14 @@ for(j in 1:nrow(meanp)){ # takes a couple minutes
 ## basic analysis
 ##################
 # most diverged loci?
-sum(selinds <- dat$p.adj <0.025 & !(dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'))) # not on Unplaced (the ones we want)
-sum(dat$p.adj <0.025 & dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12')) # within inversions
-sum(dat$p.adj <0.025 & dat$CHROM %in% c('Unplaced')) # on Unplaced
+sum(selinds <- dat$p.adj <0.1 & !(dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'))) # not on Unplaced (the ones we want)
+sum(dat$p.adj <0.1 & dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12')) # within inversions
+sum(dat$p.adj <0.1 & dat$CHROM %in% c('Unplaced')) # on Unplaced
 
 dat[selinds,]
 summary(dat$ABS_DIFF[selinds])
 
-	# find distance among loci
+	# find distance among outlier loci
 	cands <- as.data.table(dat[selinds,])
 	setkey(cands, CHROM, POS) # sort by position
 	cands[,ndist := NA] # nearest neighbor at an earlier position (measure only to left)
@@ -70,7 +73,7 @@ summary(dat$ABS_DIFF[selinds])
 	} 
 
 	selinds2 <- which(cands$ndist < 10000 & !is.na(cands$ndist)) # loci close to a previous locus
-	selinds2 <- sort(c(selinds2, selinds2-1)) # add locus before
+	selinds2 <- unique(sort(c(selinds2, selinds2-1))) # add locus before
 
 	# label each cluster
 	indx <- 1
@@ -82,7 +85,16 @@ summary(dat$ABS_DIFF[selinds])
 		}
 	}
 
-	cands[selinds2,]
+	cands[selinds2,] # the outlier loci nearby to other outlier loci
+	
+	# examine all loci in a region
+		# LG08
+	dat[CHROM=='LG08' & POS >= 16344554 & POS <=16347801,]
+	locnms11[CHROM=='LG08' & POS >= 16344554 & POS <=16347801,] # same region in 1907-2011
+
+		# LG06
+	dat[CHROM=='LG06' & POSgen >= 143400000 & POSgen <=143600000,]
+	locnms11[CHROM=='LG06' & POS >= 2780283 & POS <=2866691,] # same region in 1907-2011
 
 # write out
 write.csv(cands[selinds2,.(CHROM, POS, POSgen, locusnum, cluster, ndist, cnt1, cnt2, Freq_1, Freq_2, ABS_DIFF, p, n, pmax, p.adj)], file=paste('analysis/wfs_nullmodel_candidates', suffix, '.csv', sep=''))
@@ -111,20 +123,79 @@ write.csv(cands[selinds2,.(CHROM, POS, POSgen, locusnum, cluster, ndist, cnt1, c
 
 	# zoom in on low p-values
 	# png(width=5, height=4, filename='analysis/figures/wfs_nullmodel_hist_padjvals_zoom.png', units='in', res=300)
-	hist(dat$p.adj, col='grey', breaks=2000, xlim=c(0,0.05), ylim=c(0,1000), main='FDR-corrected p-values under null model')
+	hist(dat$p.adj, col='grey', breaks=2000, xlim=c(0,0.5), ylim=c(0,20), main='FDR-corrected p-values under null model')
 
 	dev.off()
 	
 # p-values vs. abs_diff
-	plot(dat$ABS_DIFF, -log10(dat$pmax),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=rgb(0.1,0.1,0.1, 0.2))
+	require(RColorBrewer)
+	colrmp <- colorRamp(brewer.pal(9, 'RdYlBu'))
+	colrmpfun <- function(x){
+		rgbs <- colrmp(x)
+		return(rgb(rgbs, maxColorValue=256))
+	}
+	dat[,cntmn:=2/((1/cnt1)+(1/cnt2))] # harmonic mean sample size
 
-	plot(dat$ABS_DIFF, -log10(dat$p.adj),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(FDR-adjusted p)', col=rgb(0.1,0.1,0.1, 0.2))
+	# png(width=5, height=4, filename='analysis/figures/wfs_nullmodel_pvals_vs_abs_diff.png', units='in', res=300)
+#	plot(dat$ABS_DIFF, -log10(dat$pmax),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=rgb(0.1,0.1,0.1, 0.2)) # grey
+	plot(dat$ABS_DIFF, -log10(dat$pmax),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=colrmpfun(dat[,cntmn/49])) # colored by harmonic mean sample size
+	
+	dev.off()
 
+	# png(width=5, height=4, filename='analysis/figures/wfs_nullmodel_padjvals_vs_abs_diff.png', units='in', res=300)
+#	plot(dat$ABS_DIFF, -log10(dat$p.adj),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(FDR-adjusted p)', col=rgb(0.1,0.1,0.1, 0.2))
+	plot(dat$ABS_DIFF, -log10(dat$p.adj),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(FDR-adjusted p)', col=colrmpfun(dat[,cntmn/49]))
+
+	dev.off()
 	
 # -log10(p) vs. genome position
+	quartz(width=8, height=6)
+	# png(width=8, height=6, file=paste('analysis/figures/wfs_nullmodel_p_vs_position', suffix, '.png', sep=''), res=300, units='in')
+	plot(dat$POSgen, -log10(dat$p),type='p', cex=0.2, xlab='Genome position', ylab='-log10(p)', col=rgb(0.1,0.1,0.1, 0.2))
+#		abline(h=-log10(0.0001), col='red', lty=2)
+
+		# add LG labels
+	lgs <- sort(unique(dat[,CHROM]))
+	for(j in 1:length(lgs)){
+		rng <- range(dat[CHROM==lgs[j], POSgen])
+		if(j %% 2 == 0) lines(x=rng, y=c(0,0), col=col, lwd=2)
+		if(j %% 2 == 1) lines(x=rng, y=c(0.05,0.05), col=col, lwd=2)
+		text(x=mean(rng), y=0.1, labels=lgs[j], col=col, cex=0.5)
+	
+	}
+
+		# add mean p
+	lines(meanp$mids, meanp$p, col='orange')
+
+	dev.off()
+
+
+# -log10(p) vs. genome position
+	quartz(width=8, height=6)
+	xlims=c(1.432e8, 1.439e8)
+	# png(width=8, height=6, file=paste('analysis/figures/wfs_nullmodel_p_vs_position_zoom', suffix, '.png', sep=''), res=300, units='in')
+	plot(dat$POSgen, -log10(dat$p),type='p', cex=0.2, xlab='Genome position', ylab='-log10(p)', col=rgb(0.1,0.1,0.1, 0.2), xlim=xlims)
+#		abline(h=-log10(0.0001), col='red', lty=2)
+
+		# add LG labels
+	lgs <- sort(unique(dat[,CHROM]))
+	for(j in 1:length(lgs)){
+		rng <- range(dat[CHROM==lgs[j], POSgen])
+		if(j %% 2 == 0) lines(x=rng, y=c(0,0), col=col, lwd=2)
+		if(j %% 2 == 1) lines(x=rng, y=c(0.05,0.05), col=col, lwd=2)
+		text(x=mean(rng), y=0.1, labels=lgs[j], col=col, cex=0.5)
+	
+	}
+
+		# add mean p
+	lines(meanp$mids, meanp$p, col='orange')
+
+	dev.off()
+	
+# -log10(p.adj) vs. genome position
 	col='red'
 	quartz(width=8, height=6)
-	#png(width=8, height=6, file='analysis/figures/wfs_nullmodel_p_vs_position.png', res=300, units='in')
+	# png(width=8, height=6, file=paste('analysis/figures/wfs_nullmodel_padj_vs_position', suffix, '.png', sep=''), res=300, units='in')
 	plot(dat$POSgen, -log10(dat$p.adj),type='p', cex=0.2, xlab='Genome position', ylab='-log10(FDR-adjusted p)', col=rgb(0.1,0.1,0.1, 0.2))
 		abline(h=-log10(0.05), col='red', lty=2)
 		abline(h=-log10(0.03), col='red', lty=3)
@@ -140,7 +211,34 @@ write.csv(cands[selinds2,.(CHROM, POS, POSgen, locusnum, cluster, ndist, cnt1, c
 	}
 
 		# add mean p
-	lines(meanp$mids, meanp$mean, col='orange')
+	lines(meanp$mids, meanp$p.adj, col='orange')
+
+	dev.off()
+
+
+
+# -log10(p.adj) vs. genome position (zoom)
+	col='red'
+	quartz(width=8, height=6)
+	xlims <- c(2.135e8, 2.137e8) # LG08
+	xlims <- c(1.42e8, 1.44e8) # LG06
+	# png(width=8, height=6, file=paste('analysis/figures/wfs_nullmodel_padj_vs_position_zoomLG08', suffix, '.png', sep=''), res=300, units='in')
+	plot(dat$POSgen, -log10(dat$p.adj),type='p', cex=0.2, xlab='Genome position', ylab='-log10(FDR-adjusted p)', col=rgb(0.1,0.1,0.1, 0.2), xlim=xlims, main='Zoom in on outliers')
+		abline(h=-log10(0.05), col='red', lty=2)
+		abline(h=-log10(0.03), col='red', lty=3)
+
+		# add LG labels
+	lgs <- sort(unique(dat[,CHROM]))
+	for(j in 1:length(lgs)){
+		rng <- range(dat[CHROM==lgs[j], POSgen])
+		if(j %% 2 == 0) lines(x=rng, y=c(0,0), col=col, lwd=2)
+		if(j %% 2 == 1) lines(x=rng, y=c(0.015,0.015), col=col, lwd=2)
+		text(x=mean(rng), y=0.03, labels=lgs[j], col=col, cex=0.5)
+	
+	}
+
+		# add mean p
+	lines(meanp$mids, meanp$p.adj, col='orange')
 
 	dev.off()
 
