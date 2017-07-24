82f103598064d16749d4d4f90e5184b8c4b82a67
Malin Pinsky <malin.pinsky@gmail.com>
2017-07-24 15:12:44 -0400

    Code for 1907-2011 and to investigate p-values
    Plots to understand why p-values vary across loci

--- scripts/wfs_nullmodel_analysis.r
+++ scripts/wfs_nullmodel_analysis.r
@@ -6,10 +6,29 @@
 ## Prep 
 ##########
 require(data.table)
+
+	# 1907-2014
+	# 25kmer
 locnms <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof14_25k.txt', header=TRUE); setnames(locnms, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')) # the name and observed frequencies of all the loci, from output by Bastiaan Star
 load('analysis/wfs_nullmodel_pvals_07-14_25k.rdata') # dat
 suffix <- '_07-14_25k'
-locnms11 <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof11_25k.txt', header=TRUE); setnames(locnms11, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')) # for 1907-2011 (for comparison)
+locnms11 <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof11_25k.txt', header=TRUE); 
+
+	# 150kmer
+locnms <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof14_150k.txt', header=TRUE); setnames(locnms, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')) # the name and observed frequencies of all the loci, from output by Bastiaan Star
+load('analysis/wfs_nullmodel_pvals_07-14_150k.rdata') # dat
+suffix <- '_07-14_150k'
+locnms11 <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof11_150k.txt', header=TRUE); 
+
+	# 1907-2011
+	# 25kmer
+locnms <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof11_25k.txt', header=TRUE); setnames(locnms, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')) # the name and observed frequencies of all the loci, from output by Bastiaan Star
+load('analysis/wfs_nullmodel_pvals_07-11_25k.rdata') # dat
+suffix <- '_07-11_25k'
+
+
+
+setnames(locnms11, 3:7, c('N_CHR_1', 'Freq_1', 'N_CHR_2', 'Freq_2', 'ABS_DIFF')) # for 1907-2011 (for comparison)
 
 # make a nucleotide position for the whole genome
 chrmax <- locnms[,.(len=max(POS)), by=CHROM]
@@ -22,7 +41,7 @@ locnms[,POSgen:=POS+start]
 
 # trim locus names to match rest of data (if needed)
 #locnms <- locnms[!(locnms$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),] # trim out inversions
-locnms[,locusnum:=1:nrow(locnms)] # add a locusnumber for plotting
+locnms[,locusnum:=1:nrow(locnms)] # add a locusnumber for plotting and merging
 
 	# make sure it matches
 	nrow(dat)
@@ -35,6 +54,13 @@ dat <- merge(dat, locnms[,.(locusnum, CHROM, POS, POSgen, Freq_1, Freq_2, ABS_DI
 
 # re-order columns
 setcolorder(dat, c('CHROM', 'POS', 'locusnum', 'POSgen', 'n', 'cnt1', 'cnt2', 'Freq_1', 'Freq_2', 'ABS_DIFF', 'p', 'pmax', 'p.adj'))
+
+# mask out loci with known mapping problems (25kmer)
+dat[CHROM=='LG08' & POS >= 16344554 & POS <=16347801, c("p", "pmax") :=list(NA, NA)] # polymorphic repetitive region: heterozygous individuals have 2x the coverage
+
+# re-calculate p.adj
+dat[,p.adj := p.adjust(pmax, method='fdr')]
+
 
 # write out a nice version
 write.table(dat[,.(CHROM, POS, p.adj)], file=paste('analysis/wfs_nullmodel_pos&pvals', suffix, '.txt', sep=''), quote=FALSE, sep='\t', row.names=FALSE)
@@ -50,17 +76,42 @@ for(j in 1:nrow(meanp)){ # takes a couple minutes
 	meanp$p.adj[j] <- mean(-log10(dat$p.adj[inds]))
 }
 
+######################
+# basic sanity checks
+######################
+
+# number of unique p-values for a single sample size
+numps <- dat[cnt1==46 & cnt2==44 & !is.na(p),length(unique(p)), by=.(Freq_1, Freq_2)]
+	summary(numps)
+
+# number of unique p-values for all sample sizes
+cnts <- unique(dat[,.(cnt1, cnt2)], by=c('cnt1', 'cnt2'))
+
+numps <- dat[!is.na(p) & cnt1==cnts[1,cnt1] & cnt2==cnts[1,cnt2], .(nump=length(unique(p))), by=.(Freq_1, Freq_2)]
+numps[,':=' (cnt1=cnts[1,cnt1], cnt2=cnts[1,cnt2])]
+
+for(i in 2:nrow(cnts)){
+	temp <- dat[!is.na(p) & cnt1==cnts[i,cnt1] & cnt2==cnts[i,cnt2], .(nump=length(unique(p))), by=.(Freq_1, Freq_2)]
+	temp[,':=' (cnt1=cnts[i,cnt1], cnt2=cnts[i,cnt2])]
+	numps <- rbind(numps, temp)
+}
+
+unique(numps$nump) # should all be 1
 
 ##################
 ## basic analysis
 ##################
+dat[,min(p.adj, na.rm=TRUE)]
+
 # most diverged loci?
-sum(selinds <- dat$p.adj <0.1 & !(dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'))) # not on Unplaced (the ones we want)
-sum(dat$p.adj <0.1 & dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12')) # within inversions
-sum(dat$p.adj <0.1 & dat$CHROM %in% c('Unplaced')) # on Unplaced
+pthresh <- 0.05
+sum(selinds <- dat$p.adj <pthresh & !(dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), na.rm=TRUE) # not on Unplaced (the ones we want)
+sum(dat$p.adj <pthresh & dat$CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12')) # within inversions
+sum(dat$p.adj <pthresh & dat$CHROM %in% c('Unplaced')) # on Unplaced
 
 dat[selinds,]
 summary(dat$ABS_DIFF[selinds])
+dat[selinds, table(CHROM)]
 
 	# find distance among outlier loci
 	cands <- as.data.table(dat[selinds,])
@@ -135,18 +186,85 @@ write.csv(cands[selinds2,.(CHROM, POS, POSgen, locusnum, cluster, ndist, cnt1, c
 		return(rgb(rgbs, maxColorValue=256))
 	}
 	dat[,cntmn:=2/((1/cnt1)+(1/cnt2))] # harmonic mean sample size
+	dat[,cntmin:=pmin(cnt1,cnt2)] # min sample size
 
 	# png(width=5, height=4, filename='analysis/figures/wfs_nullmodel_pvals_vs_abs_diff.png', units='in', res=300)
 #	plot(dat$ABS_DIFF, -log10(dat$pmax),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=rgb(0.1,0.1,0.1, 0.2)) # grey
 	plot(dat$ABS_DIFF, -log10(dat$pmax),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=colrmpfun(dat[,cntmn/49])) # colored by harmonic mean sample size
+#	plot(dat$ABS_DIFF, -log10(dat$pmax),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=colrmpfun(dat[,cntmin/48])) # colored by min sample size
+	plot(dat$Freq_2 - dat$Freq_1, -log10(dat$pmax),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=colrmpfun(dat[,Freq_1])) # colored Freq_1
 	
 	dev.off()
 
+	# just a single sample size
+	# png(width=5, height=4, filename='analysis/figures/wfs_nullmodel_pvals_vs_abs_diff_cnt46,44.png', units='in', res=300)
+	dat[cnt1==46 & cnt2==44, plot(ABS_DIFF, -log10(pmax), type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col=rgb(0.1,0.1,0.1,0.1), main='null model analysis, ctn1=46, cnt2=44')] # not colored
+	dat[cnt1==46 & cnt2==44, plot(Freq_2-Freq_1, -log10(pmax), type='p', cex=0.2, xlab='Frequency change', ylab='-log10(pmax)', col=colrmpfun(Freq_1), main='null model analysis, ctn1=46, cnt2=44')] # colored by Freq_1
+	dev.off()
+
+	# just a single sample size and starting frequency
+	dat[cnt1==46 & cnt2==44 & abs((Freq_2-Freq_1)-(7/46-9/44))<0.01, plot(Freq_2-Freq_1, -log10(pmax), type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col='red', main='null model analysis, ctn1=46, cnt2=44', ylim=c(0,3))]
+	dat[cnt1==46 & cnt2==44 & abs(Freq_1-6/46) < 0.01 & abs(Freq_2-2/44)<40/44, plot(Freq_2-Freq_1, -log10(pmax), type='p', cex=0.2, xlab='ABS_DIFF', ylab='-log10(p)', col='blue', main='null model analysis, ctn1=46, cnt2=44')]
+	dat[cnt1==46 & cnt2==44 & abs(Freq_1-0.3) < 0.01, points(ABS_DIFF, -log10(pmax), type='p', cex=0.2, xlab='Frequency change', ylab='-log10(p)', col='green', main='null model analysis, ctn1=46, cnt2=44')]
+
+# FDR p-values vs. abs_diff
 	# png(width=5, height=4, filename='analysis/figures/wfs_nullmodel_padjvals_vs_abs_diff.png', units='in', res=300)
 #	plot(dat$ABS_DIFF, -log10(dat$p.adj),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(FDR-adjusted p)', col=rgb(0.1,0.1,0.1, 0.2))
-	plot(dat$ABS_DIFF, -log10(dat$p.adj),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(FDR-adjusted p)', col=colrmpfun(dat[,cntmn/49]))
+	plot(dat$ABS_DIFF, -log10(dat$p.adj2),type='p', cex=0.2, xlab='Frequency change', ylab='-log10(FDR-adjusted p)', col=colrmpfun(dat[,cntmn/49]))
 
 	dev.off()
+	
+# p-value vs. Freq_1
+
+	# just a single sample size
+	quartz(width=6,height=6)
+	# png(width=5, height=4, filename=paste('analysis/figures/wfs_nullmodel_pmax_vs_Freq_1_w_Freq_2_cnt46,44', suffix, '.png', sep=''), units='in', res=300)
+	dat[cnt1==46 & cnt2==44, plot(Freq_1, -log10(pmax), type='p', cex=0.4, xlab='Initial frequency', ylab='-log10(pmax)', col=colrmpfun(Freq_2), main='null model analysis, ctn1=46, cnt2=44')]
+		# add a legend
+		points(rep(0,10), seq(3,4,length.out=10), col=colrmpfun(seq(0,1,length.out=10)), pch=16) 
+		text(rep(0.05,5), seq(3,4,length.out=5), labels=seq(0,1,length.out=5), cex=0.5)
+		text(0.025, 4.2, 'Freq_2', cex=0.8)
+
+
+# Freq_1 vs. Freq_2 with pmax
+	require(RColorBrewer)
+	colrmp <- colorRamp(brewer.pal(9, 'OrRd'))
+	colrmpfun <- function(x){
+		rgbs <- colrmp(x)
+		return(rgb(rgbs, maxColorValue=256))
+	}
+
+	# just a single sample size
+	quartz(width=6,height=6)
+	# png(width=5, height=4, filename=paste('analysis/figures/wfs_nullmodel_Freq_1_vs_Freq_2_w_pmax_cnt46,44', suffix, '.png', sep=''), units='in', res=300)
+	dat[cnt1==46 & cnt2==44, plot(Freq_1, Freq_2, type='p', cex=0.8, pch=16, xlab='Freq_1', ylab='Freq_2', col=colrmpfun(-log10(pmax)/6), main='null model analysis, cnt1=46, cnt2=44')]
+		# add a legend
+		points(rep(0,10), seq(0.6,0.8,length.out=10), col=colrmpfun(seq(0,1,length.out=10)), pch=16) 
+		text(rep(0.05,5), seq(0.6,0.8,length.out=5), labels=seq(0,6,length.out=5), cex=0.5)
+		text(-0.05, 0.85, '-log10(pmax)', cex=0.8, pos=4)
+		
+	dev.off()
+
+
+# Freq_1 vs. Freq_2 with p.adj
+	require(RColorBrewer)
+	colrmp <- colorRamp(brewer.pal(9, 'OrRd'))
+	colrmpfun <- function(x){
+		rgbs <- colrmp(x)
+		return(rgb(rgbs, maxColorValue=256))
+	}
+
+	# just a single sample size
+	quartz(width=6,height=6)
+	# png(width=5, height=4, filename=paste('analysis/figures/wfs_nullmodel_Freq_1_vs_Freq_2_w_p.adj_cnt46,44', suffix, '.png', sep=''), units='in', res=300)
+	dat[cnt1==46 & cnt2==44, plot(Freq_1, Freq_2, type='p', cex=0.8, pch=16, xlab='Freq_1', ylab='Freq_2', col=colrmpfun(-log10(p.adj)/2), main='null model analysis, cnt1=46, cnt2=44')]
+		# add a legend
+		points(rep(0,10), seq(0.6,0.8,length.out=10), col=colrmpfun(seq(0,1,length.out=10)), pch=16) 
+		text(rep(0.05,5), seq(0.6,0.8,length.out=5), labels=seq(0,2,length.out=5), cex=0.5)
+		text(-0.05, 0.85, '-log10(p.adj)', cex=0.8, pos=4)
+		
+	dev.off()
+
 	
 # -log10(p) vs. genome position
 	quartz(width=8, height=6)
