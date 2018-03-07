68eff2f423d3e02aa1a7237fa6e1f1d8729f0352
Malin Pinsky <malin.pinsky@gmail.com>
2018-03-07 11:43:15 -0500

    Apply depth filter and combine Lof & Can

--- scripts/freq_outlier_histograms.r
+++ scripts/freq_outlier_histograms.r
@@ -1,58 +1,149 @@
-## Plot allele frequency change vs. genome position
+## Plot histograms of allele frequency change and of p-values
 ## Compare across different data sets
 
 # load functions
-if(!grepl('hpc.uio.no', Sys.info()["nodename"])){ # not cod node
-	require(data.table)
-	require(plyr)
-	require(parallel)
-	ncores=2
-}
-if(grepl('hpc.uio.no', Sys.info()["nodename"])){
-	require(data.table, lib.loc="/projects/cees/lib/R_packages/")
-	require(plyr, lib.loc="/projects/cees/lib/R_packages/")
-	require(parallel, lib.loc="/projects/cees/lib/R_packages/")
-	ncores=20
-}
+require(data.table)
 
 # read in data
-dat14_25k <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof14_25k.txt', header=TRUE); setnames(dat14_25k, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_14', 'Freq_14', 'ABS_DIFF_0714')) # for 1907 vs. 2014
-dat11_25k <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof11_25k.txt', header=TRUE); setnames(dat11_25k, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_11', 'Freq_11', 'ABS_DIFF_0711')) # for 1907 vs. 2011
-dat11_14_25k <- fread('data/data_29.06.17/Frequency_table_Lof11_Lof14_25k.txt', header=TRUE); setnames(dat11_14_25k, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_11', 'Freq_11', 'ABS_DIFF_1114')) # for 1907 vs. 2011
-#datCAN <- fread('analysis/Frequency_table_Can_40_Can_TGA.txt', header=TRUE); setnames(datCAN, 3:7, c('N_CHR_40', 'Freq_40', 'N_CHR_TGA', 'Freq_TGA', 'ABS_DIFF_40TGA')) # for 1907 vs. 2011
-
-dat14_150k <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof14_150k.txt', header=TRUE); setnames(dat14_150k, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_14', 'Freq_14', 'ABS_DIFF_0714')) # for 1907 vs. 2014
-dat11_150k <- fread('data/data_29.06.17/Frequency_table_Lof07_Lof11_150k.txt', header=TRUE); setnames(dat11_150k, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_11', 'Freq_11', 'ABS_DIFF_0711')) # for 1907 vs. 2011
-dat11_14_150k <- fread('data/data_29.06.17/Frequency_table_Lof11_Lof14_150k.txt', header=TRUE); setnames(dat11_14_150k, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_11', 'Freq_11', 'ABS_DIFF_1114')) # for 1907 vs. 2011
+dat <- fread("gunzip -c analysis/wfs_nullmodel_outliers_07-11-14_Can_25k.tsv.gz")
+	dat
 
 
 #################################
 # plot histograms of frequency change
 #################################
 require(RColorBrewer)
-cols <- brewer.pal(3, 'Set1')
+cols <- brewer.pal(7, 'Set1')
 bks <- seq(0,1,by=0.01)
-hist14_25k <- dat14_25k[,hist(ABS_DIFF_0714, breaks=bks, plot=FALSE)]
-hist11_25k <- dat11_25k[,hist(ABS_DIFF_0711, breaks=bks, plot=FALSE)]
-hist11_14_25k <- dat11_14_25k[,hist(ABS_DIFF_1114, breaks=bks, plot=FALSE)]
-hist14_150k <- dat14_150k[,hist(ABS_DIFF_0714, breaks=bks, plot=FALSE)]
-hist11_150k <- dat11_150k[,hist(ABS_DIFF_0711, breaks=bks, plot=FALSE)]
-hist11_14_150k <- dat11_14_150k[,hist(ABS_DIFF_1114, breaks=bks, plot=FALSE)]
+
+# histogram
+hist14_allk <- dat[, hist(abs(Freq_14-Freq_07), breaks=bks, plot=FALSE)] # all loci
+hist11_allk <- dat[, hist(abs(Freq_11-Freq_07), breaks=bks, plot=FALSE)]
+hist11_14_allk <- dat[, hist(abs(Freq_14-Freq_11), breaks=bks, plot=FALSE)]
+histCan_allk <- dat[, hist(abs(Freq_CanMod-Freq_Can40), breaks=bks, plot=FALSE)]
+
+hist14_25k <- dat[kmer25==1, hist(abs(Freq_14-Freq_07), breaks=bks, plot=FALSE)] # 25kmer filter
+hist11_25k <- dat[kmer25==1, hist(abs(Freq_11-Freq_07), breaks=bks, plot=FALSE)]
+hist11_14_25k <- dat[kmer25==1, hist(abs(Freq_14-Freq_11), breaks=bks, plot=FALSE)]
+histCan_25k <- dat[kmer25==1, hist(abs(Freq_CanMod-Freq_Can40), breaks=bks, plot=FALSE)]
+
+hist14_25k_dp <- dat[kmer25==1 & dpLofFlag==1, hist(abs(Freq_14-Freq_07), breaks=bks, plot=FALSE)] # 25kmer and depth filter
+hist11_25k_dp <- dat[kmer25==1 & dpLofFlag==1, hist(abs(Freq_11-Freq_07), breaks=bks, plot=FALSE)]
+hist11_14_25k_dp <- dat[kmer25==1 & dpLofFlag==1, hist(abs(Freq_14-Freq_11), breaks=bks, plot=FALSE)]
+histCan_25k_dp <- dat[kmer25==1 & dpCanFlag==1, hist(abs(Freq_CanMod-Freq_Can40), breaks=bks, plot=FALSE)]
+
+hist14_25k_dp_chr <- dat[kmer25==1 & dpLofFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), hist(abs(Freq_14-Freq_07), breaks=bks, plot=FALSE)] # 25kmer, depth, and chromosome filter
+hist11_25k_dp_chr <- dat[kmer25==1 & dpLofFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), hist(abs(Freq_11-Freq_07), breaks=bks, plot=FALSE)]
+hist11_14_25k_dp_chr <- dat[kmer25==1 & dpLofFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), hist(abs(Freq_14-Freq_11), breaks=bks, plot=FALSE)]
+histCan_25k_dp_chr <- dat[kmer25==1 & dpCanFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), hist(abs(Freq_CanMod-Freq_Can40), breaks=bks, plot=FALSE)]
+
+hist14_n25k <- dat[kmer25==0, hist(abs(Freq_14-Freq_07), breaks=bks, plot=FALSE)] # fail 25kmer filter
+hist11_n25k <- dat[kmer25==0, hist(abs(Freq_11-Freq_07), breaks=bks, plot=FALSE)]
+hist11_14_n25k <- dat[kmer25==0, hist(abs(Freq_14-Freq_11), breaks=bks, plot=FALSE)]
+histCan_n25k <- dat[kmer25==0, hist(abs(Freq_CanMod-Freq_Can40), breaks=bks, plot=FALSE)]
+
+hist14_ndp <- dat[dpLofFlag==0, hist(abs(Freq_14-Freq_07), breaks=bks, plot=FALSE)] # fail depth filter
+hist11_ndp <- dat[dpLofFlag==0, hist(abs(Freq_11-Freq_07), breaks=bks, plot=FALSE)]
+hist11_14_ndp <- dat[dpLofFlag==0, hist(abs(Freq_14-Freq_11), breaks=bks, plot=FALSE)]
+histCan_ndp <- dat[dpLofFlag==0, hist(abs(Freq_CanMod-Freq_Can40), breaks=bks, plot=FALSE)]
+
+hist14_nchr <- dat[CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'), hist(abs(Freq_14-Freq_07), breaks=bks, plot=FALSE)] # fail chromosome filter
+hist11_nchr <- dat[CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'), hist(abs(Freq_11-Freq_07), breaks=bks, plot=FALSE)]
+hist11_14_nchr <- dat[CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'), hist(abs(Freq_14-Freq_11), breaks=bks, plot=FALSE)]
+histCan_nchr <- dat[CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'), hist(abs(Freq_CanMod-Freq_Can40), breaks=bks, plot=FALSE)]
+
+quartz(width=6, height=6)
+# pdf(width=6, height=6, file='figures/abs_diff_hist.pdf')
+par(mfrow=c(2,2), las=1, cex.axis=0.8, tcl=-0.2, mgp=c(2,0.5, 0), mai=c(0.5, 0.5, 0.2, 0.1))
+
+with(hist11_allk, plot(mids, density/sum(density), type='o', col=cols[1], cex=0.5, xlab='', ylab='Proportion of loci', log='y', main='1907-2011', ylim=c(1e-6, 1e-1), xlim=c(0,1)))
+with(hist11_25k, lines(mids, density/sum(density), type='o', col=cols[2], cex=0.5))
+with(hist11_25k_dp, lines(mids, density/sum(density), type='o', col=cols[3], cex=0.5))
+with(hist11_25k_dp_chr, lines(mids, density/sum(density), type='o', col=cols[4], cex=0.5))
+with(hist11_n25k, lines(mids, density/sum(density), type='o', col=cols[5], cex=0.5))
+with(hist11_ndp, lines(mids, density/sum(density), type='o', col=cols[6], cex=0.5))
+with(hist11_nchr, lines(mids, density/sum(density), type='o', col=cols[7], cex=0.5))
+#with(hist11_14_allk, lines(mids, density/sum(density), type='o', col='grey', cex=0.5))
+with(hist11_14_25k_dp_chr, lines(mids, density/sum(density), type='o', col='grey', cex=0.5))
+legend('topright', legend=c('all', '25kmer', '25k, depth', '25k, depth, chrom', 'fail 25k', 'fail depth', 'fail chrom', '2011-2014 pass'), lwd=1, pch=1, col=c(cols, 'grey'), cex=0.7, bty='n')
+
+with(hist14_allk, plot(mids, density/sum(density), type='o', col=cols[1], cex=0.5, xlab='', ylab='', log='y', main='1907-2014', ylim=c(1e-6, 1e-1), xlim=c(0,1)))
+with(hist14_25k, lines(mids, density/sum(density), type='o', col=cols[2], cex=0.5))
+with(hist14_25k_dp, lines(mids, density/sum(density), type='o', col=cols[3], cex=0.5))
+with(hist14_25k_dp_chr, lines(mids, density/sum(density), type='o', col=cols[4], cex=0.5))
+with(hist14_n25k, lines(mids, density/sum(density), type='o', col=cols[5], cex=0.5))
+with(hist14_ndp, lines(mids, density/sum(density), type='o', col=cols[6], cex=0.5))
+with(hist14_nchr, lines(mids, density/sum(density), type='o', col=cols[7], cex=0.5))
+with(hist11_14_25k_dp_chr, lines(mids, density/sum(density), type='o', col='grey', cex=0.5))
+
+with(histCan_allk, plot(mids, density/sum(density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='Proportion of loci', log='y', main='Canada', ylim=c(1e-6, 1e-1), xlim=c(0,1)))
+with(histCan_25k, lines(mids, density/sum(density), type='o', col=cols[2], cex=0.5))
+with(histCan_25k_dp, lines(mids, density/sum(density), type='o', col=cols[3], cex=0.5))
+with(histCan_25k_dp_chr, lines(mids, density/sum(density), type='o', col=cols[4], cex=0.5))
+with(histCan_n25k, lines(mids, density/sum(density), type='o', col=cols[5], cex=0.5))
+with(histCan_ndp, lines(mids, density/sum(density), type='o', col=cols[6], cex=0.5))
+with(histCan_nchr, lines(mids, density/sum(density), type='o', col=cols[7], cex=0.5))
+
+with(hist11_14_allk, plot(mids, density/sum(density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='', log='y', main='1911-2014', ylim=c(1e-6, 1e-1), xlim=c(0,1)))
+with(hist11_14_25k, lines(mids, density/sum(density), type='o', col=cols[2], cex=0.5))
+with(hist11_14_25k_dp, lines(mids, density/sum(density), type='o', col=cols[3], cex=0.5))
+with(hist11_14_25k_dp_chr, lines(mids, density/sum(density), type='o', col=cols[4], cex=0.5))
+with(hist11_14_n25k, lines(mids, density/sum(density), type='o', col=cols[5], cex=0.5))
+with(hist11_14_ndp, lines(mids, density/sum(density), type='o', col=cols[6], cex=0.5))
+with(hist11_14_nchr, lines(mids, density/sum(density), type='o', col=cols[7], cex=0.5))
 
 
-quartz(width=9, height=3)
-# pdf(width=9, height=3, file='analysis/figures/abs_diff_hist_25k&150k.pdf')
-par(mfrow=c(1,3), las=1, cex.axis=0.8, tcl=-0.2, mgp=c(2.5,0.5, 0), mai=c(0.5, 0.5, 0.2, 0.1))
+dev.off()
 
-plot(hist11_25k$mids, hist11_25k$density/sum(hist11_25k$density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='Proportion of loci', log='y', main='1907-2011', ylim=c(1e-6, 1e-1), xlim=c(0,0.6))
-lines(hist11_150k$mids, hist11_150k$density/sum(hist11_150k$density), type='o', col=cols[2], cex=0.5, main='1907-2011')
 
-plot(hist14_25k$mids, hist14_25k$density/sum(hist14_25k$density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='', log='y', main='1907-2014', ylim=c(1e-6, 1e-1), xlim=c(0,0.6))
-lines(hist14_150k$mids, hist14_150k$density/sum(hist14_150k$density), type='o', col=cols[2], cex=0.5)
 
-plot(hist11_14_25k$mids, hist11_14_25k$density/sum(hist11_14_25k$density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='', log='y', main='2011-2014', ylim=c(1e-6, 1e-1), xlim=c(0,0.6))
-lines(hist11_14_150k$mids, hist11_14_150k$density/sum(hist11_14_150k$density), type='o', col=cols[2], cex=0.5)
+#################################
+# plot histograms of combined p-values
+#################################
+require(RColorBrewer)
+cols <- brewer.pal(10, 'Paired')
+bks <- seq(0,10,by=0.25)
 
-legend('topright', legend=c('25 kmer', '150 kmer'), lwd=1, pch=1, col=cols[1:2], cex=0.8)
+# histogram
+hist_allk <- dat[, hist(-log10(p.comb), breaks=bks, plot=FALSE)] # all loci
+
+hist_25k <- dat[kmer25==1, hist(-log10(p.comb), breaks=bks, plot=FALSE)] # 25kmer filter
+
+hist_25k_dp <- dat[kmer25==1 & dpLofFlag==1, hist(-log10(p.comb), breaks=bks, plot=FALSE)] # 25kmer and depth filter
+
+hist_25k_dp_chr <- dat[kmer25==1 & dpLofFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),  hist(-log10(p.comb), breaks=bks, plot=FALSE)] # 25kmer, depth, and chromosome filter
+
+hist_n25k <- dat[kmer25==0, hist(-log10(p.comb), breaks=bks, plot=FALSE)] # fail 25kmer filter
+
+hist_ndp <- dat[dpLofFlag==0, hist(-log10(p.comb), breaks=bks, plot=FALSE)] # fail depth filter
+
+hist_nchr <- dat[CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced'), hist(-log10(p.comb), breaks=bks, plot=FALSE)] # fail chromosome filter
+hist_unpl <- dat[CHROM %in% c('Unplaced'), hist(-log10(p.comb), breaks=bks, plot=FALSE)] # unplaced
+hist_inv <- dat[CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12'), hist(-log10(p.comb), breaks=bks, plot=FALSE)] # inversions
+
+hist_25k_dp_inv <- dat[kmer25==1 & dpLofFlag==1 &CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12'), hist(-log10(p.comb), breaks=bks, plot=FALSE)] # inversions but pass kmer and depth filters
+
+#hist_null <- hist(-log10(runif(1000000,0,1)), breaks=bks, plot=FALSE) # null model: uniform (resampling approach)
+null <- data.frame(mids=seq(0.125,10,by=0.25), density=NA) # null model: exact calculation of uniform expectation with log10 bins set by bks
+	for(i in 1:nrow(null)){
+		null$density[i] <- (10^(-bks[i]) - 10^(-bks[i+1]))/(bks[i+1]-bks[i])
+	}
+
+quartz(width=5, height=5)
+# pdf(width=6, height=6, file='figures/p.comb_hist.pdf')
+par(las=1, cex.axis=0.8, tcl=-0.2, mgp=c(2.5,0.5, 0), mai=c(0.75, 0.75, 0.2, 0.1))
+
+with(hist_allk, plot(mids, density/sum(density), type='o', col=cols[1], cex=0.5, xlab='-log10(p)', ylab='Proportion of loci', log='y', main='Null model p-values', ylim=c(1e-6, 1e-0), xlim=c(0,10)))
+with(hist_25k, lines(mids, density/sum(density), type='o', col=cols[2], cex=0.5))
+with(hist_25k_dp, lines(mids, density/sum(density), type='o', col=cols[3], cex=0.5))
+with(hist_25k_dp_chr, lines(mids, density/sum(density), type='o', col=cols[4], cex=0.5))
+with(hist_n25k, lines(mids, density/sum(density), type='o', col=cols[5], cex=0.5))
+with(hist_ndp, lines(mids, density/sum(density), type='o', col=cols[6], cex=0.5))
+with(hist_nchr, lines(mids, density/sum(density), type='o', col=cols[7], cex=0.5))
+with(hist_unpl, lines(mids, density/sum(density), type='o', col=cols[8], cex=0.5))
+with(hist_inv, lines(mids, density/sum(density), type='o', col=cols[9], cex=0.5))
+with(hist_25k_dp_inv, lines(mids, density/sum(density), type='o', col=cols[10], cex=0.5))
+with(null, lines(mids, density/sum(density), type='o', col='light grey', cex=0.5, pch=4))
+legend('topright', legend=c('all', 'pass kmer', 'pass k&depth', 'pass k,dp,chr', 'fail k', 'fail dp', 'fail chr', 'unplaced', 'inversions', 'inversions pass k,dp', 'null'), lwd=1, pch=c(rep(1,10),4), col=c(cols, 'light grey'), cex=0.7, bty='n')
 
 dev.off()
--- scripts/wfs_nullmodel_analysis_Canada,1907-2011&2014.r
+++ scripts/wfs_nullmodel_analysis_Canada,1907-2011&2014.r
@@ -4,16 +4,13 @@
 require(data.table)
 
 # read in data
-	##########
-	## 25k
-	kmer='25'
 	# 1907-2011-2014
 locnms <- fread('data_2017.11.24/Frequency_table_Lof07_Lof11.txt', header=TRUE); setnames(locnms, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_11', 'Freq_11', 'ABS_DIFF')) # the name and observed frequencies of all the loci, from output by Bastiaan Star
 locnms2 <- fread('data_2017.11.24/Frequency_table_Lof07_Lof14.txt', header=TRUE); setnames(locnms2, 3:7, c('N_CHR_07', 'Freq_07', 'N_CHR_14', 'Freq_14', 'ABS_DIFF')) # the name and observed frequencies of all the loci, from output by Bastiaan Star
 	setkey(locnms, CHROM, POS, N_CHR_07, Freq_07)
 	setkey(locnms2, CHROM, POS, N_CHR_07, Freq_07)
 	locnms <- locnms[locnms2,]
-load('analysis/wfs_nullmodel_pvals_07-11-14.rdata') # dat
+load('analysis/wfs_nullmodel_pvals_07-11-14.rdata') # dat. has the p-values.
 locnms[,locusnum:=1:nrow(locnms)] # add a locusnumber for plotting and merging
 
 		# make sure it matches
@@ -39,11 +36,8 @@ locnms[,locusnum:=1:nrow(locnms)] # add a locusnumber for plotting and merging
 		datCan <- merge(dat, locnms[,.(locusnum, CHROM, POS, Freq_Can40, Freq_CanMod)], by='locusnum')
 		setnames(datCan, c('p', 'p.adj', 'cnt1', 'cnt2'), c('pCan', 'p.adjCan', 'cntCan40', 'cntCanMod'))
 
-
-
-
 ##################
-# merge
+# merge Lof and Can
 ##################
 
 setkey(datLof, CHROM, POS)
@@ -56,6 +50,8 @@ dat <- merge(datCan, datLof, all=TRUE)[, .(CHROM, POS, cnt07, cnt11, cnt14, cntC
 ################
 # add kmer25
 ################
+kmer='25' # flag for file names
+
 kmer25Lof <- fread('data_2017.11.24/Norway_25K_mer_positions.txt')
 kmer25Can <- fread('data_2017.11.24/Canada_25K_mer_positions.txt')
 
@@ -72,6 +68,34 @@ dat[is.na(kmer25), kmer25:=0] # set NAs to 0
 	nrow(dat)
 	dat[,sum(kmer25)]
 
+#########################
+# add depth statistic
+#########################
+dpLof <- fread('analysis/Outlier_sequencing_depth_statistic_All_rerun_hist.vcf.gz_HF_GQ_HWE_MISS_0.6_IND_Norway.vcf.gz.txt')
+dpCan <- fread('analysis/Outlier_sequencing_depth_statistic_All_rerun_hist.vcf.gz_HF_GQ_HWE_MISS_0.6_IND_Canada.vcf.gz.txt')
+	setnames(dpLof, c('CHROM', 'POS', 'dpstatLof'))
+	setnames(dpCan, c('CHROM', 'POS', 'dpstatCan'))
+depthstat <- merge(dpLof, dpCan, all=TRUE)
+	nrow(dpLof)
+	nrow(dpCan)
+	nrow(depthstat)
+
+dat <- merge(dat, depthstat, by=c('CHROM', 'POS'), all.x=TRUE)
+	nrow(dat)
+
+dat[,dpLofFlag := dpstatLof < quantile(dpstatLof, na.rm=TRUE, probs=0.95)]
+	dat[is.na(dpLofFlag), dpLofFlag := FALSE]
+dat[,dpCanFlag := dpstatCan < quantile(dpstatCan, na.rm=TRUE, probs=0.95)]
+	dat[is.na(dpCanFlag), dpCanFlag := FALSE]
+
+	dat[,sum(dpLofFlag)]
+	dat[,sum(dpLofFlag)]/dat[,sum(!is.na(dpstatLof))] # should be 0.95
+	dat[,sum(dpCanFlag)]
+	dat[,sum(dpCanFlag)]/dat[,sum(!is.na(dpstatCan))]
+
+dat[,dpFlag := dpLofFlag & dpCanFlag]
+	dat[,sum(dpFlag)]
+
 ###############################################
 # combine p-values for individual loci
 # https://mikelove.wordpress.com/2012/03/12/combining-p-values-fishers-method-sum-of-p-values-binomial/
@@ -82,15 +106,70 @@ dat[,p.comb := pchisq(-2 * sum(log(c(pLof, pCan))),df=2*2,lower=FALSE), by=1:nro
 # FDR-correct
 dat[,p.comb.adj := p.adjust(p.comb, method='fdr')]
 dat[!(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),p.comb.adj2 := p.adjust(p.comb, method='fdr')] # after masking out inversions and unplaced
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),p.comb.adj3 := p.adjust(p.comb, method='fdr')] # after masking out inversions and unplaced
+
+# FDR-correct each population separately
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),p.Lof.adj3 := p.adjust(pLof, method='fdr')] # after masking out inversions and unplaced
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),p.Can.adj3 := p.adjust(pCan, method='fdr')] # after masking out inversions and unplaced
 
 
 #########################
 # examine
 #########################
-# examine combined p-values
+# number of loci
+nrow(dat) # total
+dat[,sum(!is.na(cnt07))] # Lof
+dat[,sum(!is.na(cntCan40))] # Can
+dat[,sum(!is.na(cnt07) & !is.na(cntCan40))] # both
+
+nrow(dat[kmer25==1,])
+dat[kmer25==1,sum(!is.na(cnt07))] # Lof
+dat[kmer25==1,sum(!is.na(cntCan40))] # Can
+dat[kmer25==1,sum(!is.na(cnt07) & !is.na(cntCan40))] # both
+
+dat[kmer25==1 & dpLofFlag==1,sum(!is.na(cnt07))] # Lof
+dat[kmer25==1 & dpCanFlag==1,sum(!is.na(cntCan40))] # Can
+dat[kmer25==1 & dpFlag==1,sum(!is.na(cnt07) & !is.na(cntCan40))] # both
+
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),sum(!is.na(cnt07) & !is.na(cntCan40))] # both
+
+# examine combined p-values in various classes
+dat[,range(p.comb, na.rm=TRUE)]
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),range(p.comb, na.rm=TRUE)]
+
 hist(dat[,-log10(p.comb)], breaks=40, col='grey')
 
-# examine histograms for adjusted combined p-values
+	require(RColorBrewer)
+	cols <- brewer.pal(3, 'Set1')
+	bks <- seq(0,1,by=0.01)
+	hist14_25k <- dat14_25k[,hist(ABS_DIFF_0714, breaks=bks, plot=FALSE)]
+	hist11_25k <- dat11_25k[,hist(ABS_DIFF_0711, breaks=bks, plot=FALSE)]
+	hist11_14_25k <- dat11_14_25k[,hist(ABS_DIFF_1114, breaks=bks, plot=FALSE)]
+
+	quartz(width=9, height=3)
+	# pdf(width=9, height=3, file='analysis/figures/abs_diff_hist_25k&150k.pdf')
+	par(mfrow=c(1,3), las=1, cex.axis=0.8, tcl=-0.2, mgp=c(2.5,0.5, 0), mai=c(0.5, 0.5, 0.2, 0.1))
+
+	plot(hist11_25k$mids, hist11_25k$density/sum(hist11_25k$density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='Proportion of loci', log='y', main='1907-2011', ylim=c(1e-6, 1e-1), xlim=c(0,0.6))
+	lines(hist11_150k$mids, hist11_150k$density/sum(hist11_150k$density), type='o', col=cols[2], cex=0.5, main='1907-2011')
+
+	plot(hist14_25k$mids, hist14_25k$density/sum(hist14_25k$density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='', log='y', main='1907-2014', ylim=c(1e-6, 1e-1), xlim=c(0,0.6))
+	lines(hist14_150k$mids, hist14_150k$density/sum(hist14_150k$density), type='o', col=cols[2], cex=0.5)
+
+	plot(hist11_14_25k$mids, hist11_14_25k$density/sum(hist11_14_25k$density), type='o', col=cols[1], cex=0.5, xlab='Frequency change', ylab='', log='y', main='2011-2014', ylim=c(1e-6, 1e-1), xlim=c(0,0.6))
+	lines(hist11_14_150k$mids, hist11_14_150k$density/sum(hist11_14_150k$density), type='o', col=cols[2], cex=0.5)
+
+	legend('topright', legend=c('25 kmer', '150 kmer'), lwd=1, pch=1, col=cols[1:2], cex=0.8)
+
+
+# examine adjusted combined p-values
+dat[,range(p.comb.adj, na.rm=TRUE)]
+dat[,range(p.comb.adj2, na.rm=TRUE)]
+
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),range(p.comb.adj, na.rm=TRUE)]
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),range(p.comb.adj2, na.rm=TRUE)]
+dat[kmer25==1 & dpFlag==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),range(p.comb.adj3, na.rm=TRUE)]
+
 hist(dat[,-log10(p.comb.adj)], breaks=40, col='grey')
 hist(dat[,-log10(p.comb.adj2)], breaks=40, col='grey')
 
@@ -122,28 +201,41 @@ dat[kmer25==1, sum(p.comb.adj < 0.0001, na.rm=TRUE)]
 
 
 	# p.comb.adj2 < X
+dat[, sum(p.comb.adj2 < 0.1, na.rm=TRUE)]
 dat[, sum(p.comb.adj2 < 0.01, na.rm=TRUE)]
+dat[, sum(p.comb.adj2 < 0.001, na.rm=TRUE)]
 dat[, sum(p.comb.adj2 < 0.0001, na.rm=TRUE)]
+dat[, sum(p.comb.adj2 < 0.00001, na.rm=TRUE)]
 
+dat[kmer25==1, sum(p.comb.adj2 < 0.2, na.rm=TRUE)]
 dat[kmer25==1, sum(p.comb.adj2 < 0.01, na.rm=TRUE)]
 dat[kmer25==1, sum(p.comb.adj2 < 0.0001, na.rm=TRUE)]
 
+dat[kmer25==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), sum(p.comb.adj2 < 0.2, na.rm=TRUE)]
+
+	# p.comb.adj3 < X (filter on kmer25, depth, chromosome)
+dat[, sum(p.comb.adj3 < 0.2, na.rm=TRUE)]
+dat[, sum(p.comb.adj3 < 0.1, na.rm=TRUE)]
+dat[, sum(p.comb.adj3 < 0.01, na.rm=TRUE)]
+dat[, sum(p.comb.adj3 < 0.001, na.rm=TRUE)]
 
 # examine specific outliers
-dat[p.comb.adj<0.00001,]
+dat[p.comb.adj3<0.2,]
 
 
 # mark outliers based on fdr-corrected p-values (not distance to other loci)
 # don't include the inversions or unplaced or those outside kmer25
 	# combined 1907-2011 and 1907-2014
 dat[,outlier07_11_14_Can := 0]
-dat[p.comb.adj2<0.05 & kmer25==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), outlier07_11_14_Can := 1]
+dat[p.comb.adj3<0.2, outlier07_11_14_Can := 1]
 	dat[,sum(outlier07_11_14_Can, na.rm=TRUE)]
 
 
-# examine outliers
-	# combined 1907-2011-2014 and Canada
-print(dat[outlier07_11_14_Can == 1,.(CHROM, POS, Freq_07, Freq_11, Freq_14, Freq_Can40, Freq_CanMod, pLof, pCan, p.comb.adj2)], nrow=300)
+# plot outliers based on allele frequency change in both populations
+par(mfrow=c(1,2))
+dat[kmer25==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')),][sample(.N,5000), plot(abs((Freq_11 +Freq_14)/2 - Freq_07), abs(Freq_CanMod - Freq_Can40), main='all that pass filters\n(sample of 5000)', xlim=c(0,0.5), ylim=c(0,0.5))]
+dat[p.comb.adj3<0.2 & kmer25==1 & !(CHROM %in% c('LG01', 'LG02', 'LG07', 'LG12', 'Unplaced')), plot(abs((Freq_11 +Freq_14)/2 - Freq_07), abs(Freq_CanMod - Freq_Can40), main='outliers p.comb.adj2<0.05', xlim=c(0,0.5), ylim=c(0,0.5))]
+
 
 # write out tab-separated for Bastiaan
 outfile <- paste('analysis/wfs_nullmodel_outliers_07-11-14_Can_', kmer, 'k.tsv.gz', sep='')
