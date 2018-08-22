8bf802c799942e157bee0ba5527d762730dc19d4
Malin Pinsky <malin.pinsky@gmail.com>
2018-08-22 16:00:19 -0400

    Set up functions to run ABC for Lof071114

--- scripts/wfs_abc_function.r
+++ scripts/wfs_abc_function.r
@@ -1,23 +1,56 @@
 # ABC on each locus from ABC lookup table to estimate s
-# run after wfs_make_sims.r and wfs_process_sims.r
-# This version set up to run a single sample size, taken as command line arguments
+# run after wfs_make_sims.r (or wfs_make_sims_3times.r)
+# This version set up to run for Lof, Can, Pow, or PowCan for a single sample size, taken as command line arguments
+# Set up to run for a specified pair of years (07, 11, or 14) if Lof is chosen
+#	or all three years if 07 and 1114 are specified for years
+# runs on cod node as a script with arguments (myalcnt1 myalcnt2 pop myyr1 myyr2 maxcores myalcnt3)
+#	last two arguments are optional. 
 
 # read command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 
-if (length(args)<2) {
-  stop("Have to specify myalcnt1 and myalcnt2", call.=FALSE)
-} else if (length(args)==2){
+if (length(args)<5) {
+  stop("Have to specify at least myalcnt1, myalcnt2, pop, myyr1, myyr2", call.=FALSE)
+} else if (length(args)==5){
 	maxcores <- 16 # default maximum cores (for abel)
-} else if (length(args)>2) {
-	maxcores <- as.numeric(args[3])
+} else if (length(args)>5) {
+	maxcores <- as.numeric(args[6])
+} 
+if (length(args)>6) {
+	myalcnt3 <- as.numeric(args[7])
 }
+
 myalcnt1 <- as.numeric(args[1])
 myalcnt2 <- as.numeric(args[2])
+pop <- args[3]
+myyr1 <- args[4]
+myyr2 <- args[5]
 
-print(paste('myalcnt1', myalcnt1, 'myalcnt2', myalcnt2, 'maxcores', maxcores))
+if(!(pop %in% c('Lof', 'Can', 'Pow', 'PowCan'))){
+	stop('pop must be one of Lof, Can, Pow, or PowCan', call.=FALSE)
+}
+
+if(!(myyr1 %in% c('07', '11')) & pop == 'Lof'){
+	stop('myyr1 must be one of 07 or 11 if pop is Lof', call.=FALSE)
+}
+
+if(!(myyr2 %in% c('11', '14', '1114')) & pop == 'Lof'){
+	stop('myyr2 must be one of 11, 14, or 1114 if pop is Lof', call.=FALSE)
+}
+
+if(myyr2 != '1114' & exists("myalcnt3")){
+	stop('Specifying myalcnt3 only makes sense if myyr2 is 1114 (all 3 years)')
+}
+
+if(myyr2 != '1114') print(paste('myalcnt1', myalcnt1, 'myalcnt2', myalcnt2, 'pop', pop, 'myyr1', myyr1, 'myyr2', myyr2, 'maxcores', maxcores))
+if(myyr2 == '1114') print(paste('myalcnt1', myalcnt1, 'myalcnt2', myalcnt2, 'myalcnt3', myalcnt3, 'pop', pop, 'myyr1', myyr1, 'myyr2', myyr2, 'maxcores', maxcores))
 print(Sys.info()["nodename"])
+
+
+# set parameters
+tol <- 0.001 # 10k from 10M sims
+lociperpart <- 20000 # how many loci in each part (each part will be written to file)
 
 # load functions: assume this is run on a cod or abel node
 require(parallel, lib.loc="/projects/cees/lib/R_packages/")
@@ -31,19 +64,59 @@ require(ff, lib.loc="/projects/cees/lib/R_packages/") # for big objects shared a
 require(data.table, lib.loc="/projects/cees/lib/R_packages/")
 
 
-# load data
-load('analysis/wfs_targ.rdata') # targets normalized
-load('analysis/wfs_sims_meansds.rdata') # useful as a list of sample sizes, which help when loading the ff files
+# load observed data
+if(pop == 'Lof'){
+	if(myyr2 != '1114'){
+		targfile <- paste('data_2017.11.24/Frequency_table_Lof', myyr1, '_Lof', myyr2, '.txt', sep='')
+	}
+	if(myyr2 == '1114'){
+		targfile <- paste('data_2017.11.24/Frequency_table_Lof07_Lof11.txt', sep='')
+		targfile2 <- paste('data_2017.11.24/Frequency_table_Lof07_Lof14.txt', sep='')
+	}
+}
+if(pop == 'Can'){
+	targfile <- paste('data_2017.11.24/Frequency_table_CAN_40_TGA.txt', sep='')
+}
+if(pop == 'Pow'){
+	targfile <- 'analysis/Frequency_table_PowerSims_Lof_Ne46000_cnt46_44.txt'
+}
+if(pop == 'PowCan'){
+	targfile <- 'analysis/Frequency_table_PowerSims_Can_Ne5900_cnt32_40.txt'
+}
+targ <- fread(targfile, header=TRUE)
+setnames(targ, 3:7, c('alcnt1', 'f1samp', 'alcnt2', 'f2samp', 'ABS_DIFF'))
+targ[,locusnum:=1:nrow(targ)] # add a locus number indicator
 
+if(myyr2=='1114'){
+	targ2 <- fread(targfile2, header=TRUE)
+	setnames(targ2, 3:7, c('alcnt1', 'f1samp', 'alcnt3', 'f3samp', 'ABS_DIFF13'))
+	setkey(targ, CHROM, POS)
+	setkey(targ2, CHROM, POS)
+	targ <- targ[targ2,.(locusnum, CHROM, POS, alcnt1, alcnt2, alcnt3, f1samp, f2samp, f3samp)]
+}
 
-# parameters
-tol <- 0.001 # 10k from 10M sims
-lociperpart <- 100 # how many loci in each part (each part will be written to file)
+# set up file names
+existingfilespattern <- paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus*', sep='') # regexp to test for existing p-value files
+existingfilesgsubpattern <- paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus|.csv.gz', sep='') # regexp to strip out all but locus numbers from existing file names
+if(pop %in% c('Lof', 'Pow')){
+	if(myyr2 != '1114'){
+		ffnm <- paste('analysis/temp/wfs_sims_ff', paste(myalcnt1, myalcnt2, sep=','), sep='') # ff file name for Lof simulations
+	}
+	if(myyr2 == '1114'){
+		existingfilespattern <- paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, myalcnt3, sep=','), '_locus*', sep='')
+		existingfilesgsubpattern <- paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, myalcnt3, sep=','), '_locus|.csv.gz', sep='')
+ 		ffnm <- paste('analysis/temp/wfs_sims_ff', paste(myalcnt1, myalcnt2, myalcnt3, sep=','), sep='') # ff file name for Lof simulations
+	}
+}
+if(pop %in% c('Can', 'PowCan')){
+	ffnm <- paste('analysis/temp/wfs_simsCAN_ff', paste(myalcnt1, myalcnt2, sep=','), sep='')
+}
+
 
 # abc function
 # thisout.ff is an ff matrix (one slice of out[,,i])
 # assumes that thisout.ff['f1samp' and 'f2samp' are not normalized
-runabc <- function(locusnum, thistarg, thisout.ff, tol){
+runabc <- function(locusnum, thistarg, thisout.ff, tol=1/100){
 
 	# with f1samp as a summary stat
 	# calc euclidean distance
@@ -69,6 +142,34 @@ runabc <- function(locusnum, thistarg, thisout.ff, tol){
 	return(ret)
 }
 
+# for 3 timepoints
+runabc3 <- function(locusnum, thistarg, thisout.ff, tol=1/100){
+
+	# with f1samp as a summary stat
+	# calc euclidean distance
+	sum1 <- (thisout.ff['f1samp',] - thistarg[,f1samp])^2
+	sum1 <- sum1 + (thisout.ff['f2samp',] - thistarg[,f2samp])^2
+	sum1 <- sum1 + (thisout.ff['f3samp',] - thistarg[,f3samp])^2
+	dst <- sqrt(sum1)
+
+	# includes the effect of gwt in the tolerance
+	gwt <- !is.na(dst) # for now, simply define gwt as value that aren't NA
+	dst[!gwt] <- floor(max(dst[gwt])+10)
+
+	# wt1 defines the region we're interested in 
+	abstol <- quantile(dst,tol)
+	wt1 <- dst < abstol
+	if(sum(wt1)==0){ # can happen if min(dst) == abstol
+		wt1 <- dst <= abstol
+	}
+
+	# set up return matrix
+	ret <- matrix(data=c(rep(locusnum, sum(wt1)), thisout.ff['f1samp',wt1], thisout.ff['f2samp',wt1], thisout.ff['f3samp',wt1], thisout.ff['ne',wt1], thisout.ff['f1',wt1], thisout.ff['f2',wt1], thisout.ff['f3',wt1], thisout.ff['s',wt1]), ncol=7, byrow=FALSE)
+	colnames(ret) <- c('locus', 'f1samp', 'f2samp', 'f3samp', 'ne', 'f1', 'f2', 'f3', 's')
+
+	return(ret)
+
+}
 
 
 ###################################################
@@ -76,15 +177,16 @@ runabc <- function(locusnum, thistarg, thisout.ff, tol){
 ###################################################
 
 # find if any loci of this sample size have already been run (for checkpointing)
-existingfiles <- list.files(path='analysis/temp', pattern=paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus*', sep=''))
+existingfiles <- list.files(path='analysis/temp', pattern=existingfilespattern)
 print(paste('found', length(existingfiles), 'existing files relevant to this sample size'))
-existingrngs <- strsplit(gsub(paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus|.csv.gz', sep=''), '', existingfiles), split='-') # extract just the locus ranges
+existingrngs <- strsplit(gsub(existingfilesgsubpattern, '', existingfiles), split='-') # extract just the locus ranges
 existingrngs <- lapply(existingrngs, as.numeric)
 existingloci <- numeric(0)
 if(length(existingrngs)>0) for(i in 1:length(existingrngs)) existingloci <- c(existingloci, existingrngs[[i]][1]:existingrngs[[i]][2])
 
 # find the chunk of loci of the specified sample size to operate on
-samplepartinds <- targ[alcnt1==myalcnt1 & alcnt2==myalcnt2,locusnum] # locus numbers in each sample size chunk
+if(myyr2 != '1114') samplepartinds <- targ[alcnt1==myalcnt1 & alcnt2==myalcnt2,locusnum] # locus numbers in each sample size chunk
+if(myyr2 == '1114') samplepartinds <- targ[alcnt1==myalcnt1 & alcnt2==myalcnt2 & alcnt3==myalcnt3,locusnum] # locus numbers in each sample size chunk
 orig.n <- length(samplepartinds)
 samplepartinds <- setdiff(samplepartinds, existingloci) # remove loci already run (from existing files)
 sampleparts.n <- length(samplepartinds) # number of loci in the samplesize chunk
@@ -109,7 +211,6 @@ if(sampleparts.n > 0){
 	for(i in 1:length(partinds)) partinds[[i]] <- samplepartinds[((i-1)*lociperpart+1):min(c(i*lociperpart, sampleparts.n))] # indices for the loci in each part
 
 	# load the appropriate file of abc simulations
-	ffnm <- paste('analysis/temp/wfs_sims_ff', paste(myalcnt1, myalcnt2, sep=','), sep='')
 	ffload(ffnm, overwrite=TRUE) # takes 30 sec or so. loads thisout.ff
 
 	# loop through each chunk of data, writing each to file
@@ -117,13 +218,25 @@ if(sampleparts.n > 0){
 		print(paste('partnum', partnum, 'of', length(parts)))
 		thisdat <- iter(targ[locusnum %in% partinds[[partnum]],], by='row') # the iterator object: this part of the loop's chunk of data
 
-		results <- foreach(i=thisdat, .combine=rbind, .packages=c('ff', 'doParallel', 'data.table')) %dopar% {
-			runabc(locusnum=i[,locusnum], thistarg=i[,.(f1samp, f2samp)], thisout.ff=thisout.ff, tol=tol) # run abc analysis. returns vector of selected simulation parameters
+		if(myyr2 != '1114'){
+			results <- foreach(i=thisdat, .combine=rbind, .packages=c('ff', 'doParallel', 'data.table')) %dopar% {
+				runabc(locusnum=i[,locusnum], thistarg=i[,.(f1samp, f2samp)], thisout.ff=thisout.ff, tol=tol) # run abc analysis. returns vector of selected simulation parameters
+			}
+		}
+		if(myyr2 == '1114'){ # 3 time points
+			results <- foreach(i=thisdat, .combine=rbind, .packages=c('ff', 'doParallel', 'data.table')) %dopar% {
+				runabc3(locusnum=i[,locusnum], thistarg=i[,.(f1samp, f2samp, f3samp)], thisout.ff=thisout.ff, tol=tol) # run abc analysis. returns vector of selected simulation parameters
+			}
 		}
 
 		minloc <- formatC(min(partinds[[partnum]]), width=ndigits, flag='0')
 		maxloc <- formatC(max(partinds[[partnum]]), width=ndigits, flag='0')
-		outfile <- paste('analysis/temp/wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus', minloc, '-', maxloc, '.csv.gz', sep='')
+		if(myyr2 != '1114'){
+			outfile <- paste('analysis/temp/wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus', minloc, '-', maxloc, '.csv.gz', sep='')
+		}
+		if(myyr2 == '1114'){
+			outfile <- paste('analysis/temp/wfs_abc_sampsize', paste(myalcnt1, myalcnt2, myalcnt3, sep=','), '_locus', minloc, '-', maxloc, '.csv.gz', sep='')
+		}
 		write.csv(results, file=gzfile(outfile), row.names=FALSE) # write directly to gzipped file. has normalized f1samp, fsdprime, fsiprime, plus ne, f1, s
 		print(paste('wrote', outfile))
 	}
--- scripts/wfs_abc_makesbatchscript.r
+++ scripts/wfs_abc_makesbatchscript.r
@@ -8,51 +8,145 @@ if(grepl('hpc.uio.no|login', Sys.info()["nodename"])){
 }
 
 
-load('analysis/wfs_targ.rdata') # targets normalized
-load('analysis/wfs_sims_meansds.rdata') # useful as a list of sample sizes
+# settings
+#pop <- 'Lof'; myyr1 <- '07'; myyr2 <- '14'
+#pop <- 'Lof'; myyr1 <- '07'; myyr2 <- '11'
+#pop <- 'Lof'; myyr1 <- '11'; myyr2 <- '14'
+pop <- 'Lof'; myyr1 <- '07'; myyr2 <- '1114' # all 3 time points
+#pop <- 'Can'; myyr1 <- '00'; myyr2 <- '00' # myyr are placeholders since only one set of years for Canada
+
+
+# load observed data
+if(pop == 'Lof'){
+	if(myyr2 != '1114'){
+		targfile <- paste('data_2017.11.24/Frequency_table_Lof', myyr1, '_Lof', myyr2, '.txt', sep='')
+	}
+	if(myyr2 == '1114'){
+		targfile <- paste('data_2017.11.24/Frequency_table_Lof07_Lof11.txt', sep='')
+		targfile2 <- paste('data_2017.11.24/Frequency_table_Lof07_Lof14.txt', sep='')
+	}
+}
+if(pop == 'Can'){
+	targfile <- paste('data_2017.11.24/Frequency_table_CAN_40_TGA.txt', sep='')
+}
+targ <- fread(targfile, header=TRUE)
+setnames(targ, 3:7, c('alcnt1', 'f1samp', 'alcnt2', 'f2samp', 'ABS_DIFF'))
+targ[,locusnum:=1:nrow(targ)] # add a locus number indicator
+
+if(myyr2=='1114'){
+	targ2 <- fread(targfile2, header=TRUE)
+	setnames(targ2, 3:7, c('alcnt1', 'f1samp', 'alcnt3', 'f3samp', 'ABS_DIFF13'))
+	setkey(targ, CHROM, POS)
+	setkey(targ2, CHROM, POS)
+	targ <- targ[targ2,.(locusnum, CHROM, POS, alcnt1, alcnt2, alcnt3, f1samp, f2samp, f3samp)]
+}
 
 # how many loci at each sample size
-setkey(targ, alcnt1, alcnt2)
-nloci <- targ[,.(nloci=length(locusnum)), by=.(alcnt1, alcnt2)]
-	nrow(nloci) # 185... why not 193?
-	nrow(meansds)
-	setdiff(meansds[,paste(alcnt1, alcnt2)], nloci[,paste(alcnt1, alcnt2)]) # 8 sample sizes not in targ (maybe because LG01, 02, etc were trimmed out?)
+if(myyr2 != '1114'){
+	setkey(targ, alcnt1, alcnt2)
+	nloci <- targ[,.(nloci=length(locusnum)), by=.(alcnt1, alcnt2)]
+}
+if(myyr2 == '1114'){
+	setkey(targ, alcnt1, alcnt2, alcnt3)
+	nloci <- targ[,.(nloci=length(locusnum)), by=.(alcnt1, alcnt2, alcnt3)]
+}
+	nrow(nloci) # Lof 1907-2011-2014:
+				# Lof 1907-2014: 
+				# Can: 
 
 # sample sizes with >100 loci
 nloci[nloci>5000,]
 
 # check which sample sizes are already run
 nloci[,todo:=1] # set up a flag for which we should run
+nloci[,nlocitorun:=nloci] # number of loci left to run at each sample size
 
 for(i in 1:nrow(nloci)){
 	myalcnt1 <- nloci[i,alcnt1]
 	myalcnt2 <- nloci[i,alcnt2]
-	# find the chunk of loci of the specified sample size to operate on
-	samplepartinds <- targ[alcnt1==myalcnt1 & alcnt2==myalcnt2,locusnum] # locus numbers in each sample size chunk
-	orig.n <- length(samplepartinds)
+	if(myyr2=='1114') myalcnt3 <- nloci[i,alcnt3]
 
-	existingfiles <- list.files(path='analysis/temp', pattern=paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus*', sep=''))
-#	print(paste('found', length(existingfiles), 'existing file(s) relevant to this sample size'))
-	existingrngs <- strsplit(gsub(paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus|.csv.gz', sep=''), '', existingfiles), split='-') # extract just the locus ranges
-	existingrngs <- lapply(existingrngs, as.numeric)
-	existingloci <- numeric(0)
-	if(length(existingrngs)>0) for(j in 1:length(existingrngs)) existingloci <- c(existingloci, existingrngs[[j]][1]:existingrngs[[j]][2])
+	if(pop=='Lof'){
+		exist <- file.exists(paste('analysis/temp/wfs_simsnull_ff', myalcnt1, ',', myalcnt2, '.RData', sep='')) # null model sims
+		if(myyr2=='1114') exist <- file.exists(paste('analysis/temp/wfs_simsnull_ff', myalcnt1, ',', myalcnt2, ',', myalcnt3, '.RData', sep='')) # null model sims
+	}
+	if(pop=='Can'){
+		exist <- file.exists(paste('analysis/temp/wfs_simsnullCAN_ff', myalcnt1, ',', myalcnt2, '.RData', sep='')) # null model sims	
+	}
 
-	samplepartinds <- setdiff(samplepartinds, existingloci) # remove loci already run (from existing files)
-	sampleparts.n <- length(samplepartinds) # number of loci in the samplesize chunk
+	if(exist){
+		# find the chunk of loci of the specified sample size to operate on
+		samplepartinds <- targ[alcnt1==myalcnt1 & alcnt2==myalcnt2,locusnum] # locus numbers in each sample size chunk
+		if(myyr2=='1114') samplepartinds <- targ[alcnt1==myalcnt1 & alcnt2==myalcnt2 & alcnt3==myalcnt3,locusnum]
+		orig.n <- length(samplepartinds)
 
-	if(sampleparts.n==0){
-		nloci[i,todo:=0]
+		# check for existing files (don't re-run these)
+		if(myyr2 != '1114'){
+			existingfiles <- list.files(path='analysis/temp', pattern=paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus*', sep=''))
+			existingrngs <- strsplit(gsub(paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, sep=','), '_locus|.csv.gz', sep=''), '', existingfiles), split='-') # extract just the locus ranges
+		}
+		if(myyr2=='1114'){ # 3 time points
+			existingfiles <- list.files(path='analysis/temp', pattern=paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, myalcnt3, sep=','), '_locus*', sep=''))
+			existingrngs <- strsplit(gsub(paste('wfs_abc_sampsize', paste(myalcnt1, myalcnt2, myalcnt3, sep=','), '_locus|.csv.gz', sep=''), '', existingfiles), split='-') # extract just the locus ranges
+		}
+
+		existingrngs <- lapply(existingrngs, as.numeric)
+		existingloci <- numeric(0)
+		if(length(existingrngs)>0) for(j in 1:length(existingrngs)) existingloci <- c(existingloci, existingrngs[[j]][1]:existingrngs[[j]][2])
+
+		# find the set of loci remaining to run
+		samplepartinds <- setdiff(samplepartinds, existingloci) # remove loci already run (from existing files)
+		sampleparts.n <- length(samplepartinds) # number of loci in the samplesize chunk
+
+		# flag which ones still need to be run
+		if(sampleparts.n==0){
+			nloci[i,todo:=0]
+			nloci[i,nlocitorun:=0]
+		} else {
+			if(myyr2 != '1114') print(paste(myalcnt1, myalcnt2, 'Run', sampleparts.n, 'loci of', orig.n, 'loci originally'))
+			if(myyr2 == '1114') print(paste(myalcnt1, myalcnt2, myalcnt3, 'Run', sampleparts.n, 'loci of', orig.n, 'loci originally'))
+			nloci[i,nlocitorun:=sampleparts.n]
+		}
 	} else {
-		print(paste(myalcnt1, myalcnt2, 'Run', sampleparts.n, 'loci of', orig.n, 'loci originally'))
+		nloci[i,todo:=0] # can't run this now because sim files don't exist
+		print(paste(myalcnt1, myalcnt2, 'Cannot run because not all sim files exist'))
+		warning(paste(myalcnt1, myalcnt2, 'Cannot run because not all sim files exist'))
 	}
 }
+
+# how many sample sizes to run?
+sum(nloci$todo)
+nrow(nloci) # compare to number we could run
 	
-# write out the shell script
-outfile <- 'scripts/wfs_abc_submit_all_sbatch2.sh'
+# any sample sizes with < all loci to run?
+nloci[nlocitorun<nloci,]
 
-cat('#!/bin/bash\n', file=outfile, append=FALSE) # header
+# trim nloci to rows to do
+nloci2 <- nloci[todo==1,]
 
-for(i in which(nloci[,todo==1])){
-	cat(paste('sbatch --job-name=abc', nloci[i,alcnt1], ',', nloci[i,alcnt2], ' --cpus-per-task=', min(16, nloci[i,nloci]), ' scripts/wfs_abc_sbatch.sh ', nloci[i,alcnt1], ' ', nloci[i, alcnt2], ' ', min(16, nloci[i,nloci]), '\n', sep=''), file=outfile, append=TRUE)
+# break into batches of <= 400
+nbatch <- ceiling(nrow(nloci2)/400)
+nloci2[,batch:=0]
+for(i in 1:nbatch){
+	rows <- ((i-1)*400+1):min(i*400, nrow(nloci2))
+	nloci2[rows,batch:=i]
+}
+
+# write out the shell script for each batch
+for(j in 1:nbatch){
+	outfile <- paste('scripts/wfs_abc_submit_all_sbatch', j, '.sh', sep='')
+	print(paste('Wrote', outfile))
+
+	cat('#!/bin/bash\n', file=outfile, append=FALSE) # header
+
+	if(myyr2 != '1114'){
+		for(i in which(nloci2[,batch==j])){
+			cat(paste('sbatch --job-name=abc', nloci2[i,alcnt1], ',', nloci2[i,alcnt2], ' --cpus-per-task=', min(16, nloci2[i,nloci]), ' scripts/wfs_abc_sbatch.sh ', nloci2[i,alcnt1], ' ', nloci2[i, alcnt2], ' ', pop, ' ', myyr1, ' ', myyr2, ' ', min(16, nloci2[i,nloci]), '\n', sep=''), file=outfile, append=TRUE)
+		}
+	}
+	if(myyr2 == '1114'){ # 3 time points
+		for(i in which(nloci2[,batch==j])){
+			cat(paste('sbatch --job-name=abc', nloci2[i,alcnt1], ',', nloci2[i,alcnt2], ',', nloci2[i,alcnt3], ' --cpus-per-task=', min(16, nloci2[i,nloci]), ' scripts/wfs_abc_sbatch_3times.sh ', nloci2[i,alcnt1], ' ', nloci2[i, alcnt2], ' ', pop, ' ', myyr1, ' ', myyr2, ' ', min(16, nloci2[i,nloci]), ' ', nloci2[i,alcnt3], '\n', sep=''), file=outfile, append=TRUE)
+		}
+	}
 }
