cd4202ee5563ca38e4b5017c4a99871f74273573
Malin Pinsky <malin.pinsky@gmail.com>
2019-05-24 17:35:49 -0400

    Compare region change statistics and output outlier region list

--- scripts/region_change_compare.r
+++ scripts/region_change_compare.r
@@ -1,7 +1,12 @@
 # Compare freq change, LD change, Tajima's D change, pi change
 
+require(data.table)
+
+################################
 # read in data
-width='1e4'
+################################
+
+wd <- 1e4; width='1e4'
 freq <- readRDS('analysis/Frequency_table_ABS_DIFF_runmean1e4.rds')
 pi <- readRDS('analysis/pi_change_region_1e4.rds')
 tajd <- readRDS('analysis/tajimasD_change_region_1e4.rds')
@@ -204,3 +209,53 @@ bins[ld_region10kb_percCAN<=0.99 & (pi_region10kb_percCAN>0.995 | pi_region10kb_
 bins[ld_region10kb_percCAN>0.99 & (pi_region10kb_percCAN>0.995 | pi_region10kb_percCAN<0.005), points(ld_diff_CAN, pi_diff_CAN, cex=0.4, col=cols[3], pch=16)]
 
 dev.off()
+
+
+#####################
+# Find outlier regions
+#####################
+
+# regions with large frequency change and large increase in LD
+bins[,freqLD0711 := freq_region10kb_perc0711 + ld_region10kb_perc0711]
+bins[,freqLD0714 := freq_region10kb_perc0714 + ld_region10kb_perc0714]
+bins[,freqLDCAN := freq_region10kb_percCAN + ld_region10kb_percCAN]
+	
+	# 07-11
+	setkey(bins, freqLD0711); bins[freq_region10kb_perc0711>0.99 & ld_region10kb_perc0711>0.99,.(CHROM, BIN_START, freq_diff_0711, ld_diff_0711, freqLD0711, freq_region10kb_perc0711, ld_region10kb_perc0711)]
+	# 07-14
+	setkey(bins, freqLD0714); bins[freq_region10kb_perc0714>0.99 & ld_region10kb_perc0714>0.99,.(CHROM, BIN_START, freq_diff_0714, ld_diff_0714, freqLD0714, freq_region10kb_perc0714, ld_region10kb_perc0714)]
+	# 07-11 and 07-14
+	setkey(bins, freqLD0711, freqLD0714); bins[freq_region10kb_perc0711>0.95 & ld_region10kb_perc0711>0.95 & freq_region10kb_perc0714>0.95 & ld_region10kb_perc0714>0.95,.(CHROM, BIN_START, freq_diff_0711, freq_diff_0714, ld_diff_0711, ld_diff_0714, freqLD0711, freqLD0714)]
+	# CAN
+	setkey(bins, freqLDCAN); bins[freq_region10kb_percCAN>0.99 & ld_region10kb_percCAN>0.99,.(CHROM, BIN_START, freq_diff_CAN, ld_diff_CAN, freqLDCAN, freq_region10kb_percCAN, ld_region10kb_percCAN)]
+	# 07-11 and 07-14 and CAN
+	setkey(bins, freqLD0711, freqLD0714, freqLDCAN); bins[freq_region10kb_perc0711>0.95 & ld_region10kb_perc0711>0.95 & freq_region10kb_perc0714>0.95 & ld_region10kb_perc0714>0.95 & freq_region10kb_percCAN>0.95 & ld_region10kb_percCAN>0.95,.(CHROM, BIN_START, freq_diff_0711, freq_diff_0714, freq_diff_CAN, ld_diff_0711, ld_diff_0714, ld_diff_CAN)]
+
+	par(mfrow=c(1,3)); bins[,plot(freqLD0711, freqLD0714, cex=0.2, col='#00000033')]; bins[,plot(freqLD0711, freqLDCAN, cex=0.2, col='#00000033')]; bins[,plot(freqLD0714, freqLDCAN, cex=0.2, col='#00000033')]
+	bins[,cor.test(freqLD0711, freqLD0714)]
+	bins[,cor.test(freqLD0711, freqLDCAN)]
+	bins[,cor.test(freqLDCAN, freqLD0714)]
+	
+	
+	# write out a selection of highly ranked loci
+	setkey(bins, CHROM, BIN_START)
+
+	write.csv(bins[(freq_region10kb_perc0711>0.99 & ld_region10kb_perc0711>0.99) | (freq_region10kb_perc0714>0.99 & ld_region10kb_perc0714>0.99) | (freq_region10kb_percCAN>0.99 & ld_region10kb_percCAN>0.99), .(CHROM, BIN_START, BIN_END=BIN_START+wd, freqLD0711, freqLD0714, freqLDCAN, freq_diff_0711, freq_diff_0714, freq_diff_CAN, ld_diff_0711, ld_diff_0714, ld_diff_CAN)], file='analysis/outlier_10kregions_freqLD_07-11-14_Can.csv')
+	
+	
+# rank the outlier regions based in clustering them together (see Oziolor et al. 2019 Science)
+outlreg0711 = bins[,.(CHROM=unique(CHROM), BIN_START=min(BIN_START), BIN_END=max(BIN_START+wd), WIDTH=max(BIN_START+wd)-min(BIN_START), score=sum(freq_diff_0711)), by=freq_region10kb_cluster0711]
+outlreg0714 = bins[,.(CHROM=unique(CHROM), BIN_START=min(BIN_START), BIN_END=max(BIN_START+wd), WIDTH=max(BIN_START+wd)-min(BIN_START), score=sum(freq_diff_0714)), by=freq_region10kb_cluster0714]
+outlregCAN = bins[,.(CHROM=unique(CHROM), BIN_START=min(BIN_START), BIN_END=max(BIN_START+wd), WIDTH=max(BIN_START+wd)-min(BIN_START), score=sum(freq_diff_CAN)), by=freq_region10kb_clusterCAN]
+
+setkey(outlreg0711, score)
+setkey(outlreg0714, score)
+setkey(outlregCAN, score)
+
+outlreg0711[,sort(unique(WIDTH))] # cluster width always 10kb
+outlreg0714[,sort(unique(WIDTH))]
+outlregCAN[,sort(unique(WIDTH))]
+
+tail(outlreg0711, 10)
+tail(outlreg0714, 10)
+tail(outlregCAN, 10)
