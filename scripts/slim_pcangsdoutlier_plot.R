f4538917b8d197123c8522e26972342d0a54cb63
Malin Pinsky <malin.pinsky@gmail.com>
2020-06-11 10:35:22 +0200

    Plot pcangsd analyses on new 20 Slim sims

--- scripts/slim_pcangsdoutlier_plot.R
+++ scripts/slim_pcangsdoutlier_plot.R
@@ -31,12 +31,9 @@ for(i in 1:length(files)){
   sitestemp[, c('CHROM', 'POS') := tstrsplit(V1, '_', fixed = TRUE)] # split chromosome and position
   temp <- cbind(temp, sitestemp[, .(CHROM, POS)])
 
-  # calculate p-value and FDR correct (whole genome)
+  # calculate p-value and FDR correct
   temp[, p := pchisq(q = stat, df = 1, lower.tail = FALSE)]
   temp[, pfdr := p.adjust(p, method = 'fdr')]
-
-  # FDR correct (only chrom with selection)
-  temp[CHROM==1, pfdr_chr1 := p.adjust(p, method = 'fdr')]
 
   # calc num sites, trim to p<0.001 (or at least one site with the min FDR)
   temp[, npos := .N]
@@ -50,12 +47,15 @@ for(i in 1:length(files)){
   temp[, ne := as.numeric(gsub('slim_sim_n|_s.*', '', files[i]))] # get Ne from file name
   temp[, s := as.numeric(gsub('slim_sim_n[[:digit:]]*_s|_f.*', '', files[i]))] # get s
   temp[, f := as.numeric(gsub('slim_sim_n[[:digit:]]*_s[[:digit:].]*_f|_i.*', '', files[i]))] # get min f
-  temp[, i := as.numeric(gsub('slim_sim_n[[:digit:]]*_s[[:digit:].]*_f0\\.[[:digit:]]*_i|\\.selection.npy', '', files[i]))] # get iteration
+  temp[, i := as.numeric(gsub('slim_sim_n[[:digit:]]*_s[[:digit:].]*_f0\\.[[:digit:]]*_i|\\.selection.npy|\\_comb.selection.npy', '', files[i]))] # get iteration
+  combval <- ifelse(grepl('comb', files[i]), 1, 0) # not sure why, but this fails if inside the data.table expression
+  temp[, comb := combval] # get whether chromsome has been combined with 20 s=0 chromosomes
   
   npctemp[, ne := temp$ne[1]]
   npctemp[, s := temp$s[1]]
   npctemp[, f := temp$f[1]]
   npctemp[, i := temp$i[1]]
+  npctemp[, comb := temp$comb[1]]
   
   if(i == 1){
     outl <- temp
@@ -77,58 +77,58 @@ dim(outl)
 
 # stats on the region under selection
 sum1 <- outl[, .(minp = min(p), minpfdr = min(pfdr), npfdrlow = sum(pfdr < 0.05), npos = unique(npos)),  # min p and pfdr
-              by = .(ne, s, f, i)]
+              by = .(ne, s, f, i, comb)]
 
-sum2 <- sum1[, .(prop = sum(minpfdr <= 0.05)/.N), by = .(ne, s, f)] # proportion of sims that detected at least one SNP under selection
+sum2 <- sum1[, .(prop = sum(minpfdr <= 0.05)/.N, npos = mean(npos)), by = .(ne, s, f, comb)] # proportion of sims that detected at least one SNP under selection
 ############
 # plot
 ############
 
-# quick plot of each sim. now too many to visualize well.
+# plot of each sim. now too many to visualize well. VERY SLOW (hours)
 p <- ggplot(outl, aes(x=POS, y= -log10(pfdr))) + 
   geom_point(data = outl[-log10(pfdr) >= -log10(0.05),], size = 0.2, shape = 1, alpha = 0.3, color = 'red') +
   geom_point(data = outl[-log10(pfdr) < -log10(0.05),], size = 0.2, shape = 1, alpha = 0.3, color = 'black') +
-  facet_grid(ne + i ~ s + f)
+  facet_grid(ne + i ~ s + f + comb)
   #geom_hline(yintercept= -log10(0.05), linetype="dashed")
-ggsave('figures/slim_pcangsdoutlier.png', plot = p, width = 8, height = 32, dpi = 300)
+ggsave('figures/slim_pcangsdoutlier.png', plot = p, width = 16, height = 32, dpi = 300)
 
 
 
-# plot min p vs. s and ne (group by f)
+# plot min p vs. s and ne and comb (group by f)
 ggplot(sum1, aes(x = s, y = -log10(minp), group = as.factor(f), color = as.factor(f))) +
   geom_point(size = 0.5) +
   geom_smooth(method = 'lm', size = 0.5) +
-  facet_grid(~ ne) +
+  facet_grid(comb ~ ne) +
   labs(color = 'Initial frequency') +
   ylab('Min -log10 p-value')
-ggsave('figures/slim_pcangsdoutlier_minp.png', width = 7, height = 2, dpi = 150)
+ggsave('figures/slim_pcangsdoutlier_minp.png', width = 7, height = 4, dpi = 150)
 
-# plot min pfdr vs. s and ne (group by f)
+# plot min pfdr vs. s and ne and comb (group by f)
 ggplot(sum1, aes(x = s, y = -log10(minpfdr), group = as.factor(f), color = as.factor(f))) +
   geom_point(size = 0.5) +
   geom_smooth(method = 'lm', size = 0.5) +
-  facet_grid(~ ne) +
+  facet_grid(comb ~ ne) +
   labs(color = 'Initial frequency') +
   ylab('Min -log10\nFDR-corrected p-value') + 
   geom_hline(yintercept= -log10(0.05), linetype="dashed")
-ggsave('figures/slim_pcangsdoutlier_minpfdr.png', width = 7, height = 2, dpi = 150)
+ggsave('figures/slim_pcangsdoutlier_minpfdr.png', width = 7, height = 4, dpi = 150)
 
 
 # plot number of pfdr<0.05 vs. s and ne (group by f)
 ggplot(sum1, aes(x = s, y = log10(npfdrlow+1), group = as.factor(f), color = as.factor(f))) +
   geom_point(size = 0.5) +
 #  geom_smooth(method = 'lm', size = 0.5) +
-  facet_grid(~ ne) +
+  facet_grid(comb ~ ne) +
   labs(color = 'Initial frequency') +
-  ylab('log10(# of pfdr<0.05 + 1')
-ggsave('figures/slim_pcangsdoutlier_npfdrlow.png', width = 7, height = 2, dpi = 150)
+  ylab('log10(# of pfdr<0.05 + 1)')
+ggsave('figures/slim_pcangsdoutlier_npfdrlow.png', width = 7, height = 4, dpi = 150)
 
 # plot proportion of sims that have at least one pfdr<0.05 vs. s and ne (group by f)
 ggplot(sum2, aes(x = s, y = prop, group = f, color = as.factor(f))) +
   geom_point(data = sum2[f == 0.05,], size = 3) +
   geom_point(data = sum2[f == 0.2,], size = 1) +
 #  geom_smooth(method = 'lm', size = 0.5) +
-  facet_grid(~ ne) +
+  facet_grid(comb ~ ne) +
   labs(color = 'Initial frequency') +
   ylab('Proportion min FDR-corrected p < 0.05')
-ggsave('figures/slim_pcangsdoutlier_propminpfdrlow.png', width = 7, height = 2, dpi = 150)
+ggsave('figures/slim_pcangsdoutlier_propminpfdrlow.png', width = 7, height = 4, dpi = 150)
