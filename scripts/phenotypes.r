a020aaab095e14834f78e7769678cb84ea2f3604
Malin Pinsky <malin.pinsky@gmail.com>
2020-12-08 10:25:26 -0500

    Analyze Lofoten age at maturity through 2020

--- scripts/phenotypes.r
+++ scripts/phenotypes.r
@@ -12,21 +12,40 @@ calc50p <- function(x,y){ # fit logistic regression and return x value for y=50%
 
 # read in data
 canmat <- fread('data/phenotypes/Brattey et al_2018_Table18_proportion_mature.csv')
+lofmat <- fread('data/phenotypes/AFWG_Table 3.11.csv', skip = 3)
+lofmat <- lofmat[!is.na(Year_age), ] # trim off an NA row
 
 # long format
 canmatl <- melt(canmat, id.vars = c('Year'), value.name = 'propmat')
 canmatl[, age := as.numeric(gsub('Age', '', variable))]
 
+setnames(lofmat, c('Year_age', '+gp'), c('Year', '15')) # set older ages to 15 for simplicity
+lofmatl <- melt(lofmat, id.vars = c('Year'), value.name = 'propmat', variable.name = 'age') # gives an error, but I don't understand why. looks fine
+lofmatl[, age := as.numeric(age)]
+
 # plot maturity curves by year
 ggplot(canmatl, aes(age, propmat, group = Year, color = Year)) +
   geom_line(alpha = 0.5)
 
+ggplot(lofmatl, aes(age, propmat, group = Year, color = Year)) +
+  geom_line(alpha = 0.5)
+
+
 # calc 50% maturity from logistic regression models
-mat50 <- canmatl[, .(age50 = calc50p(age, cbind(propmat*1000, (1-propmat)*1000))), by = Year]
+canmat50 <- canmatl[, .(age50 = calc50p(age, cbind(propmat*1000, (1-propmat)*1000)), pop = 'can'), by = Year]
+lofmat50 <- lofmatl[, .(age50 = calc50p(age, cbind(propmat*1000, (1-propmat)*1000)), pop = 'lof'), by = Year]
 
 # plot age of 50% maturity by year
-ggplot(mat50, aes(Year, age50)) +
-  geom_line()
+ggplot(canmat50, aes(Year, age50)) +
+  geom_line() + 
+  geom_smooth()
+
+ggplot(lofmat50, aes(Year, age50)) +
+  geom_line() + 
+  geom_smooth()
+
+# combine
+mat50 <- rbind(canmat50, lofmat50)
 
 # write out
-write.csv(mat50, 'output/age_50percmature_can.csv', row.names = FALSE)
+write.csv(mat50, 'output/age_50percmature.csv', row.names = FALSE)
