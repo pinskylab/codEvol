028227cbf091a17124c72a8f15e6d39890023f28
Malin Pinsky <malin.pinsky@gmail.com>
2021-02-21 14:37:28 -0500

    Remove unused scripts and figures.

--- scripts/wfs_abc_compare_rej_vs_regr.r
+++
@@ -1,7 +0,0 @@
-# compare rejection vs. regression ABC
-
-sampsize <- '50,48' # choose the set of simulations
-
-filesregr <- list.files(path='analysis/temp', pattern=paste('wfs_abcregr_sampsize', sampsize, sep=''), full.names=TRUE)
-
-posts <- read.csv(gzfile(filesregr[1]))
